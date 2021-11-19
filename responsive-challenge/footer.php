98386d050689f89733cd75cc2e588d1b54f6b98b
Tara <tara.haskamp@gmail.com>
2021-11-18 20:36:01 -0600

    Remove projects folder to clean folder structure

---
+++ responsive-challenge/footer.php
@@ -0,0 +1,12 @@
+</main>
+
+		<footer class="site-footer">
+			<div class="inner-column">
+				<nav>
+					<a href="#">Home</a>
+					<a href="#info">Info</a>
+				</nav>
+			</div>
+		</footer>
+	</body>
+</html>
---
+++ responsive-challenge/index.php
@@ -0,0 +1,72 @@
+<?php include('header.php');?>
+
+<section class="about">
+
+	<svg class='ellipses' height="150" width="500">
+	  <ellipse cx="240" cy="100" rx="220" ry="30"/>
+	  <ellipse cx="220" cy="70" rx="190" ry="20"/>
+	  <ellipse cx="210" cy="45" rx="170" ry="15"/>
+	</svg>
+
+	<div class="inner-column">
+		<img src="https://www.prestigeanimalhospital.com/sites/default/files/interesting-cat-facts.jpg"></img>
+
+		<h1>Cats have personalities as unique and interesting as our human friends!</h1>
+	</div>
+
+	<svg class='rectangle' width="400" height="180">
+	  <rect x="50" y="20" rx="20" ry="20" width="150" height="150"/>
+	</svg>
+</section>
+
+<section class="content">
+	<div class="inner-column">
+		<content>
+			<article class="info">
+				<img src="https://pettraxinc.com/wp-content/uploads/2018/10/catlazy2.jpg">
+
+				<section class="info-content">
+					<h2>Laid Back</h2>
+
+					<p>Sometimes cats are super laid back and just lie around the house all day. Many you will find near a window or door soaking up the sunshine and warmth. Some are even avid bird watchers.
+					</p>
+
+					<a href="https://www.vox.com/future-perfect/21536524/cats-dogs-feline-philosophy-john-gray">Wisdom</a>
+			</article>
+
+			<article class="info">
+				<img src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2019%2F05%2Fgrumpy-cat-2-2000.jpg">
+
+				<section class="info-content">
+					<h2>Grumpy Cats</h2>
+
+					<p>The late, beloved, Grumpy Cat is the face of how many see all feline friends. Mad at the world and full of hiss and vinegar. Contrary to that face, Tardar Sauce (her actual name) was a sweet and friendly little ball of fluff.
+					</p>
+
+					<a href="https://www.treehugger.com/grumpy-cat-isnt-so-cranky-after-all-4864049">Cranky</a>
+				</section>
+			</article>
+
+			<article class="info">
+				<img src="https://www.purina.com/sites/g/files/auxxlc196/files/styles/kraken_generic_max_width_480/public/Abyssinian_body_7.jpg?itok=oi_7JqLe">
+
+				<section class="info-content">
+					<h2>Playful Kitties!</h2>
+
+					<p>Who doesn't love to watch a little furball bat a little jingling ball across the floor?? Or darting back and forth trying to catch the red dot of a laser pointer. They exhibit such a carefree playfulness right before stretching out into a catnap.
+					</p>
+
+					<a href="https://www.israel21c.org/cat2see-lets-you-play-with-your-cat-remotely/">Play!</a>
+				</section>
+			</article>
+		</content>
+	</div>
+
+	<svg class='ellipses' height="150" width="500">
+	  <ellipse cx="240" cy="100" rx="220" ry="30"/>
+	  <ellipse cx="220" cy="70" rx="190" ry="20"/>
+	  <ellipse cx="210" cy="45" rx="170" ry="15"/>
+	</svg>
+</section>
+
+<?php include ('footer.php'); ?>
