6b9f901a952bf481907564574f1130f93f44ccca
Tara <tara.haskamp@gmail.com>
2021-11-19 22:18:58 -0600

    Add about and project section to index

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include('header.php');?>
+
+<section class="about">
+	<div class="inner-column">
+		<h1>United Sites of Tara</h1>
+
+		<h2>Also a little info <em>about</em> Tara</h2>
+
+		<p>Currently a student/mentee/learner at Perpetual Education, learning all about the wonderful world of web design and development. Ultimately my goal is to obtain a job as a front end or back end web developer.</p>
+
+		<p>This is my "business card" site used as a portfolio of sorts to showcase the work that I am currently working on or have completed.</p>
+
+		<p>It is a constant work in progress and will be updated as more projects are completed.</p>
+	</div>
+</section>
+
+<?php include('projects.php');?>
+		
+
+
+</html>
---
+++ projects.php
@@ -0,0 +1,79 @@
+	<section class='projects'>
+		<div class='inner-column'>
+			<h1>My Projects</h1>
+			
+			<section class="boilerplate">
+				<h2>Boilerplate</h2>
+
+				<a href="boilerplate/index.html">Boilerplate</a>
+
+				<p>This project was to set up a basic boilerplate template to use for a quick start to future projects. It incorporates HTML, metadata and a basix index setup. It also uses CSS, including a Meyer Reset doc.</p>
+			</section>
+
+			<section class="challenge1">
+				<h2>First Challenge</h2>
+
+				<a href="challenge-1/index.html">Challenge #1</a>
+
+				<p>This was our first challenge - to set up a basic folder structure to be able to use relative file names. This makes it so that both local and online versions will work correctly and can easily be adjusted.</p>
+			</section>
+
+			<section class="adventure">
+				<h2>Choose Your Own Adventure</h2>
+
+				<a href="choose-adventure/1.html">Choose Your adventure!</a>
+
+				<p>This was an extra project that I decided to take on and have some fun with my son. This exercise helped to instill file structure as well as mapping out paths to understand the best way to set up a website with multiple pages.</p>
+			</section>
+
+			<section class="mock-client">
+				<h2>Mock Client - Homebodies</h2>
+
+				<a href="homebodies/index.html">Homebodies</a>
+
+				<p>This was our first project working with a "client" that had their own wishes and individual needs for their business. I chose a personal training couple who helps to customize home gyms as well as provide workout plans. This involved asking questions and making sure the essential functions were met.</p>
+			</section>
+
+			<section class="mountains">
+				<h2>Mountain Layout</h2>
+
+				<a href="layout-challenge/index.html">Layout Challenge</a>
+
+				<p>This was to help us familiarize ourselves with the @media query in CSS to adjust how a website looks based on the width of the browser.</p>
+			</section>
+
+			<section class="responsive">
+				<h2>Responsive Layout Challenge</h2>
+
+				<a href="layout-challenge1113/index.html">Responsive Layout Challenge</a>
+
+				<p>This was our chance to show our understanding of Flex Box and positioning. Using the @media query to adjust for browser width, we made functional changes at each breakpoint, including some background flourishes.</p>
+			</section>
+
+			<section class="responsive-2">
+				<h2>Responsive Layout Refresh</h2>
+
+				<a href="responsive-challenge/index.php">Responsive Challenge</a>
+
+				<p>After going through the Responsive Layout Challenge and facing multiple hurdles, I decided to have a second attempt to clean up some of the html and CSS from the first attempt. This time it was completed much quicker and all of the code was much more tidy. I also incorporated PHP to streamline the html even further</p>
+			</section>
+
+			<section class="replica">
+				<h2>Website Replica</h2>
+
+				<a href="website-replica/index.html">Website Replica</a>
+
+				<p>We were challenged to find a website to replicate with a fairly limited HTML and CSS knowledge. We hadn't learned flex box or positioning at this point. I chose to do a local restaurant that I enjoy. <a href='https://www.thirdstreetsocial.com/'>Here</a> is the original website that mine is based on.</p>
+			</section>
+
+			<section class="four-page">
+				<h2>Four Page</h2>
+
+				<a class='here' href="challenge-1/projects/four-page-with-style/index.html">Updated 4 Page</a>
+
+				<p>This was another project for building basic folder structure.</p>
+			</section>
+			
+		</div>
+	</section>
+</main>
