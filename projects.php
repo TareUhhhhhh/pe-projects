0093d129ab5b373ddc29e236ca088fa449547097
Tara <tara.haskamp@gmail.com>
2021-11-23 20:48:19 -0600

    Add targets to anchors

--- projects.php
+++ projects.php
@@ -1,17 +1,17 @@
-	<section class='projects'>
+	<section class='projects' id='projects'>
 		<div class='inner-column'>
 			<h1>My Projects</h1>
 
 			<section class="adventure">
 				<h2>Choose Your Own Adventure</h2>
 
-				<a href="choose-adventure/1.html">Choose Your adventure!</a>
+				<a href="choose-adventure/1.html" target='choose-adventure'>Choose Your Adventure!</a>
 
 				<p>This was an extra project that I decided to take on and have some fun with my son. This exercise helped to instill file structure as well as mapping out paths to understand the best way to set up a website with multiple pages.</p>
 			</section>
 
 			<section class="custom-properties">
-				<h2>Custom Properties and Light/Dark theme</h2>
+				<h2>Custom Properties and Light/Dark Theme</h2>
 
 				<a href="custom-properties-code" target='custom-properties-code'>Custom Properties</a>
 
@@ -21,7 +21,7 @@
 			<section class="mock-client">
 				<h2>Mock Client - Homebodies</h2>
 
-				<a href="homebodies/index.html">Homebodies</a>
+				<a href="homebodies" target='homebodies'>Homebodies</a>
 
 				<p>This was our first project working with a "client" that had their own wishes and individual needs for their business. I chose a personal training couple who helps to customize home gyms as well as provide workout plans. This involved asking questions and making sure the essential functions were met.</p>
 			</section>
@@ -29,7 +29,7 @@
 			<section class="mountains">
 				<h2>Mountain Layout</h2>
 
-				<a href="layout-challenge/index.html">Layout Challenge</a>
+				<a href="layout-challenge" target='layout-challenge'>Layout Challenge</a>
 
 				<p>This was to help us familiarize ourselves with the @media query in CSS to adjust how a website looks based on the width of the browser.</p>
 			</section>
@@ -37,20 +37,18 @@
 			<section class="fell-over">
 				<h2>Flexbox Module Fun!</h2>
 
-				<a href="flexbox-module-fun/index.html">I Fell Over!</a>
+				<a href="flexbox-module-fun" target='flexbox-module-fun'>I Fell Over!</a>
 
 				<p>When we first learned about Flex Box, we were also told about the *secret* that we can create our own elements. So I got a little creative and made a person! I also used the media query to have a little fun when the browser adjusted.</p>
 			</section>
 
 			<section class="responsive">
-				<h2>Responsive Layout Challenge</h2>
+				<h2>Responsive Layout</h2>
 
-				<a href="layout-challenge1113/index.html">Responsive Layout Challenge</a>
+				<a href="layout-challenge1113" target='responsive'>Responsive Layout Challenge</a>
 
 				<p>This was our chance to show our understanding of Flex Box and positioning. Using the @media query to adjust for browser width, we made functional changes at each breakpoint, including some background flourishes.</p>
-			</section>
 
-			<section class="responsive-2">
 				<h2>Responsive Layout Refresh</h2>
 
 				<a href="responsive-challenge/index.php">Responsive Challenge</a>
@@ -61,18 +59,18 @@
 			<section class="replica">
 				<h2>Website Replica</h2>
 
-				<a href="website-replica/index.html">Website Replica</a>
+				<a href="website-replica" target='replica'>Website Replica</a>
 
 				<p>We were challenged to find a website to replicate with a fairly limited HTML and CSS knowledge. We hadn't learned flex box or positioning at this point. I chose to do a local restaurant that I enjoy. <a href='https://www.thirdstreetsocial.com/'>Here</a> is the original website that mine is based on.</p>
 			</section>
 
-			<section class="four-page">
+		<!-- 	<section class="four-page">
 				<h2>Four Page</h2>
 
-				<a class='here' href="challenge-1/projects/four-page-with-style/index.html">Updated 4 Page</a>
+				<a class='here' href="challenge-1/projects/four-page-with-style">Updated 4 Page</a>
 
 				<p>This was another project for building basic folder structure.</p>
-			</section>
+			</section> -->
 			
 		</div>
 	</section>
