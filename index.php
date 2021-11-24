ae4df9da72e2524c884067b924e8cf16785cf947
Tara <tara.haskamp@gmail.com>
2021-11-23 20:46:32 -0600

    Add Contact section and minor verbiage changes

--- index.php
+++ index.php
@@ -1,21 +1,35 @@
 		<?php include('header.php');?>
 
-		<section class="about">
+		<section class="about" id='about'>
 			<div class="inner-column">
-				<h1>United Sites of Tara</h1>
+				<h1>The United Sites of Tara</h1>
 
-				<h2>Also a little info <em>about</em> Tara</h2>
+				<h2>A little info <em>about</em> Tara</h2>
 
 				<p>Currently a student/mentee/learner at Perpetual Education, learning all about the wonderful world of web design and development. Ultimately my goal is to obtain a job as a front end or back end web developer.</p>
 
-				<p>This is my "business card" site used as a portfolio of sorts to showcase the work that I am currently working on or have completed.</p>
+				<p>This is my "business card" site used as a portfolio of sorts to showcase the work that I am currently working on or have finished.</p>
 
-				<p>It is a constant work in progress and will be updated as more projects are completed.</p>
+				<p>It is a constant work in progress and will be updated as more projects are completed!</p>
 			</div>
 		</section>
 
 		<?php include('projects.php');?>
-			
+
+		<section class="contact" id='contact'>
+			<div class="inner-column">
+				<h1>Reach Out!</h1>
+
+				<h3>Questions? Feedback? Want to know more or just chat about website development?</h3>
+
+				<p>Email: tara.haskamp@gmail.com</p>
+
+				<p>GitHub: <a href='https://github.com/TareUhhhhhh'>TareUhhhhhh</a></p>
+			</div>
+		</section>
+
+		<?php include('footer.php');?>
+
 	</body>
 
 </html>
