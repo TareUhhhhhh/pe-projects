00e37fbda51b1ce02aca180eba313c130468ac96
Tara <tara.haskamp@gmail.com>
2021-11-23 20:54:21 -0600

    Add Contact link to the header of portfolio page

--- header.php
+++ header.php
@@ -17,7 +17,10 @@
 			<div class="inner-column">
 				<nav>
 					<a href='#about'>About Tara</a>
+					
 					<a href='#projects'>Projects</a>
+
+					<a href='#contact'>Contact</a>
 				</nav>
 			</div>
 		</header>
