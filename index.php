0fc9bd04f260735d42908d70fa9eec26913f7e7c
Tara <tara.haskamp@gmail.com>
2021-12-02 22:08:53 -0600

    Add more elements to apply flex box

--- index.php
+++ index.php
@@ -4,13 +4,17 @@
 			<div class="inner-column">
 				<h1>The United Sites of Tara</h1>
 
-				<h2>A little info <em>about</em> Tara</h2>
+				<div class="console">
+					<h2>A little info <em>about</em> Tara</h2>
+					
+					<content>
+						<p>Currently a student / mentee / learner at Perpetual Education, learning all about the wonderful world of web design and development. Ultimately my goal is to obtain a job as a front end or back end web developer.</p>
 
-				<p>Currently a student/mentee/learner at Perpetual Education, learning all about the wonderful world of web design and development. Ultimately my goal is to obtain a job as a front end or back end web developer.</p>
+						<p>This is my "business card" site used as a portfolio of sorts to showcase the work that I am currently working on or have finished.</p>
 
-				<p>This is my "business card" site used as a portfolio of sorts to showcase the work that I am currently working on or have finished.</p>
-
-				<p>It is a constant work in progress and will be updated as more projects are completed!</p>
+						<p>It is a constant work in progress and will be updated as more projects are completed!</p>
+					</content>
+				</div>
 			</div>
 		</section>
 
@@ -24,7 +28,7 @@
 
 				<p>Email: tara.haskamp@gmail.com</p>
 
-				<p>GitHub: <a href='https://github.com/TareUhhhhhh'>TareUhhhhhh</a></p>
+				<p>GitHub: <a href='https://github.com/TareUhhhhhh' target='https://github.com/TareUhhhhhh'>TareUhhhhhh</a></p>
 			</div>
 		</section>
 
