d5851e48b93063af64ee5d8e4e41a3ed9feeecff
Tara <tara.haskamp@gmail.com>
2021-12-21 16:23:29 -0600

    Separate head and contact files

---
+++ contact.php
@@ -0,0 +1,15 @@
+<?php include('head.php');?>
+<?php include('header.php');?>
+
+	<section class="contact" id='contact'>
+			<div class="inner-column">
+				<h1>Reach Out!</h1>
+
+				<p>Questions? Feedback? Want to know more or just chat about website development?</p>
+
+				<p>Email: tara.haskamp@gmail.com</p>
+
+				<p>GitHub: <a href='https://github.com/TareUhhhhhh' target='https://github.com/TareUhhhhhh'>TareUhhhhhh</a></p>
+			</div>
+	</section>
+
