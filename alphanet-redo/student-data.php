fee21e57459f063691cfbc486c34ac3db13620b5
Tara <tara.haskamp@gmail.com>
2022-01-05 16:20:00 -0600

    Create separate student-data file

---
+++ alphanet-redo/student-data.php
@@ -0,0 +1,22 @@
+<?php
+
+	$students = [
+		[
+			"id" => "tara",
+			"name" => "TareUhhhhhh",
+			"intro" => "A person who makes things.",
+			"link" => "Check it out!",
+		],
+		[
+			"id" => "folder-name",
+			"name" => "Your Name HERE!",
+			"intro" => "Tell us why we should check out your site.",
+			"link" => "Clicky clack",
+		],
+		[
+			"id" => "sheriff",
+			"name" => "Sheriff Derek",
+			"intro" => "Someone's got to design this town.",
+			"link" => "Giddy Up",
+		]
+	];
