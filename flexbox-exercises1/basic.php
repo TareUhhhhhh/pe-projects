1d81893d7018d2916795aa8bd4399aa853554b16
Tara <tara.haskamp@gmail.com>
2022-01-05 17:33:03 -0600

    Create basic flexbox php file

---
+++ flexbox-exercises1/basic.php
@@ -0,0 +1,103 @@
+<?php ?>
+
+<h1>Basic Layouts</h1>
+
+<article class='one'>
+				<div class='blue'>One</div>
+
+				<div class='red'>Two</div>
+
+				<div class='orange'>Three</div>
+
+				<div class='yellow'>Four</div>
+			</article>
+
+			<article class='two'>
+				<div class='blue'>One</div>
+
+				<div class='red'>Two</div>
+
+				<div class='orange'>Three</div>
+
+				<div class='yellow'>Four</div>
+			</article>
+
+			<article class='three'>
+				<div class='blue'>One</div>
+
+				<div class='red'>Two</div>
+
+				<div class='orange'>Three</div>
+
+				<div class='yellow'>Four</div>
+			</article>
+
+			<article class='four'>
+				<div class='blue'>One</div>
+
+				<div class='red'>Two</div>
+
+				<div class='orange'>Three</div>
+
+				<div class='yellow'>Four</div>
+			</article>
+
+			<article class='five'>
+				<div class='blue'>One</div>
+
+				<div class='red'>Two</div>
+
+				<div class='orange'>Three</div>
+
+				<div class='yellow'>Four</div>
+			</article>
+
+			<article class='six'>
+				<div class='blue'>One</div>
+
+				<div class='red'>Two</div>
+
+				<div class='orange'>Three</div>
+
+				<div class='yellow'>Four</div>
+			</article>
+
+			<article class='seven'>
+				<div class='blue'>One</div>
+
+				<div class='red'>Two</div>
+
+				<div class='orange'>Three</div>
+
+				<div class='yellow'>Four</div>
+			</article>
+
+			<article class='eight'>
+				<div class='blue'>One</div>
+
+				<div class='red'>Two</div>
+
+				<div class='orange'>Three</div>
+
+				<div class='yellow'>Four</div>
+			</article>
+
+			<article class='nine'>
+				<div class='blue'>One</div>
+
+				<div class='red'>Two</div>
+
+				<div class='orange'>Three</div>
+
+				<div class='yellow'>Four</div>
+			</article>
+
+			<article class='ten'>
+				<div class='blue'>One</div>
+
+				<div class='red'>Two</div>
+
+				<div class='orange'>Three</div>
+
+				<div class='yellow'>Four</div>
+			</article>
