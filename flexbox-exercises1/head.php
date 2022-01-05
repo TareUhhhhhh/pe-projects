193efd4b8e9b1c2f75362bb0853fd09a77c4e800
Tara <tara.haskamp@gmail.com>
2022-01-05 17:32:39 -0600

    Create head php file

---
+++ flexbox-exercises1/head.php
@@ -0,0 +1,11 @@
+<!doctype html>
+
+<html lang='en'>
+	<head>
+		<meta charset='utf-8'>
+		<meta name='viewport' content='width=device-width, initial-scale=1'>
+		<title>Flex-Box Exercises 1</title>
+		<meta name='description' content='First Flexbox Exercise'>
+		<meta property='og:image' content='images/default.png'>
+		<link rel='stylesheet' href='css/style.css'>
+	</head>
