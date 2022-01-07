7be66d6efe913d18ddca1ba3082f89acb9a6e74f
Tara <tara.haskamp@gmail.com>
2022-01-07 17:47:51 -0600

    Create Great Names file

--- responsive-php/great-names.php
+++ responsive-php/great-names.php
@@ -1,1 +1,50 @@
-great-names.php
+<?php include('header.php'); ?>
+
+<section class="goofy">
+	<div class="inner-column">
+		<h1>Goofy Cat Names</h1>
+
+		<ul>
+			<li>Baby</li>
+			<li>Bugs</li>
+			<li>Sassy</li>
+			<li>Garfield</li>
+			<li>Tinkerbell</li>
+		</ul>
+	</div>
+</section>
+
+<section class="food-names">
+	<div class="inner-column">
+		<h1>Food Names</h1>
+
+		<ul>
+			<li>Taco</li>
+			<li>Tuna</li>
+			<li>Nugget</li>
+			<li>Tardar Sauce (RIP Grumpy Cat)</li>
+		</ul>
+	</div>
+</section>
+	
+<section class="human-names">
+	<div class="inner-column">
+		<h1>Human Names</h1>
+
+		<!-- <h2>...Because there are some silly (and awesome) humans out there who love giving their cats human names.</h2> -->
+
+		<ul>
+			<li>Maddie</li>
+			<li>Bailey</li>
+			<li>Daphne</li>
+			<li>Fiona</li>
+			<li>Brody</li>
+			<li>Alfred</li>
+			<li>Hershel</li>
+			<li>Samantha</li>
+		</ul>
+	</div>
+</section>
+
+
+<?php include('footer.php'); ?>
