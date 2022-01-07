f44c43d4a5c5f0f4fd9638870c475ea80d0080cd
Tara <tara.haskamp@gmail.com>
2022-01-07 17:46:44 -0600

    Create Necessities page

---
+++ responsive-php/necessities.php
@@ -0,0 +1,28 @@
+<?php include('header.php'); ?>
+
+<section class="noms">
+	<div class="inner-column">
+		<h1>We all gotta eat something!</h1>
+		<ul>
+			<li>Food (wet or dry)</li>
+			<li>Treats!</li>
+			<li>Food and water bowls</li>
+			<li>Fresh water daily</li>
+		</ul>
+	</div>
+</section>
+
+<section class="others">
+	<div class="inner-column">
+		<h1>Other stuff</h1>
+		<ul>
+			<li>Litter box</li>
+			<li>Pooper Scooper (or get an automatic box!)</li>
+			<li>TOYS</li>
+			<li>A cozy bed</li>
+			<li>Collar with name tag and your contact info</li>
+		</ul>
+	</div>
+</section>
+
+<?php include('footer.php'); ?>
