1d1983cfba4260ff02d2986674844db4fa08299f
Tara <tara.haskamp@gmail.com>
2022-01-06 21:49:35 -0600

    Create head php file

---
+++ alphanet-redo/head.php
@@ -0,0 +1,13 @@
+<!doctype html>
+	<html lang='en'>
+		<head>
+			<meta charset='utf-8'>
+			<meta name='viewport' content='width=device-width, initial-scale=1'>
+			<title>The A-Threem</title>
+			<meta name='description' content='A Rag-Tag group with some websites to check out.'>
+			<meta property='og:image' content='images/default.png'>
+			<link rel='stylesheet' href='css/style.css'>
+			<link href="https://fonts.googleapis.com/css?family=Dosis|Titillium+Web" rel="stylesheet">
+		</head>
+
+		<body>
