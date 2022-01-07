616df8e17fd6a89294f3177a732fd904bfa72100
Tara <tara.haskamp@gmail.com>
2022-01-06 23:14:03 -0600

    Create challenge 2 php and css

---
+++ flexbox-exercises1/challenge2.php
@@ -0,0 +1,33 @@
+<?php ?>
+
+<article class='challenge2'>
+	<div class="yellow1">
+		<div class="green1">
+			<div class="pink1">
+				<div class="blue1"></div>
+				<div class="blue2"></div>
+			</div>
+			<div class="pink2"></div>
+		</div>
+		<div class="green2">
+			<div class="pink3">
+				<div class="blue3">
+					<div class="red"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="yellow2">
+		<div class="green3">
+			<div class="pink4"></div>
+		</div>
+		<div class="green4">
+			<div class="pink5">
+				<div class="blue4"></div>
+			</div>
+			<div class="pink6"></div>
+			<div class="pink7"></div>
+		</div>
+	</div>
+</article>
