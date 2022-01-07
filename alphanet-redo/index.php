57deb6259fba2c7d77fa2eaa400a4a22fe7ffce8
Tara <tara.haskamp@gmail.com>
2022-01-06 21:49:57 -0600

    Add html and link head file

--- alphanet-redo/index.php
+++ alphanet-redo/index.php
@@ -1,22 +1,23 @@
-<?php include('student-data.php');
-?>
 
-<link rel="stylesheet" href="style.css">
+<?php include('head.php'); ?>
+<?php include('student-data.php'); ?>
+	<header>
+		<h1>Net of the <nobr>A-Threem</nobr></h1>
+	</header>
 
-<h1>The Net of the A-Threem</h1>
+		<main>
+			<?php foreach ($students as $student) { ?>
+				<article class='student <?= $student["id"] ?>'>
+					<h1><?= $student["name"] ?></h1>
 
-<main>
-	<?php foreach ($students as $student) { ?>
-		<article class='student <?= $student["id"] ?>'>
-			<h1><?= $student["name"] ?></h1>
+					<p><?= $student["intro"] ?></p>
 
-			<p><?= $student["intro"] ?></p>
-
-			<a href="https://peprojects.dev/alpha-3/<?= $student["id"] ?>"><?= $student["link"] ?></a>
-		</article>
-	<?php } ?>
-</main>
-
+					<a href="https://peprojects.dev/alpha-3/<?= $student["id"] ?>"><?= $student["link"] ?></a>
+				</article>
+			<?php } ?>
+		</main>
+	</body>
+</html>
 
 
 
