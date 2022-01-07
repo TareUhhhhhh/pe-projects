d51ed5fd0d062e0e8b500130581c882cc26cfcfe
Tara <tara.haskamp@gmail.com>
2022-01-07 16:20:46 -0600

    Create responsive site duplicate with PHP

---
+++ responsive-php/header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+
+<html lang='en'>
+	<head>
+		<meta charset='utf-8'>
+		<meta name='viewport' content='width=device-width, initial-scale=1'>
+		<title>Responsive Layout Challenge...with CATS!</title>
+		<meta name='description' content='Responsive Layout Challenge...with CATS!'>
+		<meta property='og:image' content='images/default.png'>
+		<link rel='stylesheet' href='css/style.css'>
+	</head>
+
+	<body>
+		<header class="site-header">
+			<div class="inner-column">
+				<section class="header-content">
+					<img class='logo' id='home' src="https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d">
+
+					<nav class='header-nav'>
+						<a href='#'>Home</a>
+						<a href='.info'>Info</a>
+					</nav>
+				</section>
+			</div>
+		</header>
+
+		<main>
