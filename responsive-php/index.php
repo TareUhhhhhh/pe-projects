f6a8dbfd76401f987da1bf1c804412b1748919e9
Tara <tara.haskamp@gmail.com>
2022-01-08 21:21:59 -0600

    Split the header and intro to separate php files

--- responsive-php/index.php
+++ responsive-php/index.php
@@ -1,20 +1,5 @@
-<?php include('header.php');?>
-
-<section class="about">
-	<svg class='ellipses' height="150" width="500">
-	  <ellipse cx="140" cy="100" rx="80" ry="80"/>
-	</svg>
-
-	<div class="inner-column">
-		<img src="https://www.prestigeanimalhospital.com/sites/default/files/interesting-cat-facts.jpg"></img>
-
-		<h1>Cats have personalities as unique and interesting as our human friends!</h1>
-	</div>
-
-	<svg class='rectangle' width="400" height="180">
-	  <ellipse cx="240" cy="90" rx="80" ry="80"/>
-	</svg>
-</section>
+<?php include('header.php'); ?>
+<?php include('intro.php'); ?>
 
 <section class="content" id='content'>
 	<div class="inner-column">
@@ -61,8 +46,7 @@
 	</div>
 
 	<svg class='ellipses' height="150" width="500">
-	  <ellipse cx="340" cy="80" rx="80" ry="80"/>
-	</svg>
+	  <ellipse cx="340" cy="80" rx="80" ry="80"/></svg>
 </section>
 
 <section class="more-goodies">
---
+++ responsive-php/intro.php
@@ -0,0 +1,15 @@
+<section class="about">
+	<svg class='ellipses' height="150" width="500">
+	  <ellipse cx="140" cy="100" rx="80" ry="80"/>
+	</svg>
+
+	<div class="inner-column">
+		<img src="https://www.prestigeanimalhospital.com/sites/default/files/interesting-cat-facts.jpg"></img>
+
+		<h1>Cats have personalities as unique and interesting as our human friends!</h1>
+	</div>
+
+	<svg class='rectangle' width="400" height="180">
+	  <ellipse cx="240" cy="90" rx="80" ry="80"/>
+	</svg>
+</section>
