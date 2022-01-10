142dfc58af4cefce8d579609a7a20d1b0a31afd8
Tara <tara.haskamp@gmail.com>
2022-01-10 17:18:32 -0600

    Separate articles from index php

---
+++ projects/responsive-php/additional.php
@@ -0,0 +1,13 @@
+<?php ?>
+
+	<article class='great-names'>
+			<h2>For some great feline name ideas, check out these resources.</h2>
+
+		<a href="great-names.php">Great Names!</a>
+	</article>
+
+	<article class='necessities'>
+			<h2>When you bring a new feline friend into your family, there are a lot of items they need!</h2>
+
+		<a href="necessities.php">Don't forget the poop scooper! (Or do!)</a>
+	</article>
--- projects/responsive-php/index.php
+++ projects/responsive-php/index.php
@@ -4,44 +4,7 @@
 <section class="content" id='content'>
 	<div class="inner-column">
 		<content>
-			<article class="info">
-				<img src="https://pettraxinc.com/wp-content/uploads/2018/10/catlazy2.jpg">
-
-				<section class="info-content">
-					<h2>Laid Back</h2>
-
-					<p>Sometimes cats are super laid back and just lie around the house all day. Many you will find near a window or door soaking up the sunshine and warmth. Some are even avid bird watchers.
-					</p>
-
-					<a href="https://www.vox.com/future-perfect/21536524/cats-dogs-feline-philosophy-john-gray">Wisdom</a>
-				</section>
-			</article>
-
-			<article class="info">
-				<img src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2019%2F05%2Fgrumpy-cat-2-2000.jpg">
-
-				<section class="info-content">
-					<h2>Grumpy Cats</h2>
-
-					<p>The late, beloved, Grumpy Cat is the face of how many see all feline friends. Mad at the world and full of hiss and vinegar. Contrary to that face, Tardar Sauce (her actual name) was a sweet and friendly little ball of fluff.
-					</p>
-
-					<a href="https://www.treehugger.com/grumpy-cat-isnt-so-cranky-after-all-4864049">Cranky</a>
-				</section>
-			</article>
-
-			<article class="info">
-				<img src="https://www.purina.com/sites/g/files/auxxlc196/files/styles/kraken_generic_max_width_480/public/Abyssinian_body_7.jpg?itok=oi_7JqLe">
-
-				<section class="info-content">
-					<h2>Playful Kitties!</h2>
-
-					<p>Who doesn't love to watch a little furball bat a little jingling ball across the floor?? Or darting back and forth trying to catch the red dot of a laser pointer. They exhibit such a carefree playfulness right before stretching out into a catnap.
-					</p>
-
-					<a href="https://www.israel21c.org/cat2see-lets-you-play-with-your-cat-remotely/">Play!</a>
-				</section>
-			</article>
+			<?php include('articles.php'); ?>
 		</content>
 	</div>
 
@@ -51,17 +14,7 @@
 
 <section class="more-goodies">
 	<div class="inner-column">
-		<article class='great-names'>
-			<h2>For some great feline name ideas, check out these resources.</h2>
-
-		<a href="great-names.php">Great Names!</a>
-		</article>
-
-		<article class='necessities'>
-			<h2>When you bring a new feline friend into your family, there are a lot of items they need!</h2>
-
-		<a href="necessities.php">Don't forget the poop scooper! (Or do!)</a>
-		</article>
+		<?php include('additional.php'); ?>
 	</div>
 </section>
 	
