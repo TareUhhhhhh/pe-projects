a553baaa60d05ffdcc1e0c64b24514db501bc1fa
Tara <tara.haskamp@gmail.com>
2022-01-10 17:15:30 -0600

    Create projects folder index file

---
+++ projects/projects.php
@@ -0,0 +1,83 @@
+<?php include('head.php');?>
+<?php include('header.php');?>
+
+<section class='projects' id='projects'>
+	<div class='inner-column'>
+		<h1>My Projects</h1>
+
+		<section class="adventure">
+			<div class="item-title">
+				<h2>Choose Your Own Adventure</h2>
+
+				<a href="choose-adventure/1.html" target='choose-adventure'>Choose Your Adventure!</a>
+			</div>
+
+			<div class="item-content">
+				<p>This was an extra project that I decided to take on and have some fun with my son. This exercise helped to instill file structure as well as mapping out paths to understand the best way to set up a website with multiple pages.</p>
+
+				<p>I also used this as an opportunity to use consistent CSS stylesheets throughout the game. There are standard styles, losing styles and winning styles for each! Having one standard stylesheet means that I can easily update multiple pages in one stylesheet.</p>
+			</div>
+		</section>
+
+		<section class="custom-properties">
+			<div class="item-title">
+				<h2>Custom Properties and Light/Dark Theme</h2>
+
+				<a href="custom-properties-code" target='custom-properties-code'>Custom Properties</a>
+			</div>
+
+			<p>Intro to custom properties in CSS. This was an exciting new tool to add to my arsenal! Not only can I easily incorporate a set theme of colors throughout (and easily update them), but I can also more easily adjust for light and dark mode user preferences!</p>
+		</section>
+
+		<section class="mock-client">
+			<div class="item-title">
+				<h2>Homebodies</h2>
+
+				<a href="homebodies" target='homebodies'>Homebodies</a>
+			</div>
+
+			<p>This was our first project working with a "client" that had their own wishes and individual needs for their business. I chose a personal training couple who helps to customize home gyms as well as provide workout plans. This involved asking questions and making sure the essential functions were met.</p>
+		</section>
+
+		<section class="mountains">
+			<div class="item-title">
+				<h2>Mountain Layout</h2>
+
+				<a href="layout-challenge" target='layout-challenge'>Layout Challenge</a>
+			</div>
+
+			<p>This was to help us familiarize ourselves with the @media query in CSS to adjust how a website looks based on the width of the browser.</p>
+		</section>
+
+		<section class="fell-over">
+			<div class="item-title">
+				<h2>Flexbox Module Fun!</h2>
+
+				<a href="flexbox-module-fun" target='flexbox-module-fun'>I Fell Over!</a>
+			</div>
+
+			<p>When we first learned about Flex Box, we were also told about the *secret* that we can create our own elements. So I got a little creative and made a person! I also used the media query to have a little fun when the browser adjusted.</p>
+		</section>
+
+		<section class="responsive">
+
+			<div class="item-title">
+				<h2>Responsive Layout Refresh</h2>
+
+				<a href="responsive-challenge/index.php">Responsive Challenge</a>
+			</div>
+
+			<p>After going through the Responsive Layout Challenge and facing multiple hurdles, I decided to have a second attempt to clean up some of the html and CSS from the first attempt. This time it was completed much quicker and all of the code was much more tidy. I also incorporated PHP to streamline the html even further</p>
+		</section>
+
+		<section class="replica">
+			<div class="item-title">
+				<h2>Website Replica</h2>
+
+				<a href="website-replica" target='replica'>Website Replica</a>
+			</div>
+
+			<p>We were challenged to find a website to replicate with a fairly limited HTML and CSS knowledge. We hadn't learned flex box or positioning at this point. I chose to do a local restaurant that I enjoy. <a href='https://www.thirdstreetsocial.com/'>Here</a> is the original website that mine is based on.</p>
+		</section>
+	</div>
+</section>
