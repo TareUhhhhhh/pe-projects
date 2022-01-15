01296043e6d4f47cbc3e4ff1d388d484e51727bc
Tara <tara.haskamp@gmail.com>
2022-01-14 22:40:07 -0600

    Add meta data info and image

--- projects/layout-theme-challenge-tara/head.php
+++ projects/layout-theme-challenge-tara/head.php
@@ -2,9 +2,9 @@
 	<meta charset='utf-8'>
 	<meta name='viewport' content='width=device-width, initial-scale=1'>
 
-	<title>Super theme challenge</title>
-	<meta name='description' content='[[insert description]]'>
-	<meta property='og:image' content='images/default.png'>
+	<title>TareUhhhhhh Gaming</title>
+	<meta name='description' content='The Spookiest and Most Exciting Games Around'>
+	<meta property='og:image' content='images/skel.jpg'>
 
 	<link rel='stylesheet' href='css/style.css'>
 </head>
