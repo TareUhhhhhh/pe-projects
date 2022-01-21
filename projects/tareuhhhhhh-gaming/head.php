cebe384854e8a13f5d60370769f54bd425671880
Tara <tara.haskamp@gmail.com>
2022-01-20 20:15:02 -0600

    Add site metadata

--- projects/tareuhhhhhh-gaming/head.php
+++ projects/tareuhhhhhh-gaming/head.php
@@ -2,9 +2,9 @@
 	<meta charset='utf-8'>
 	<meta name='viewport' content='width=device-width, initial-scale=1'>
 
-	<title>Super theme challenge</title>
-	<meta name='description' content='[[insert description]]'>
-	<meta property='og:image' content='images/default.png'>
+	<title>TareUhhhhhh Gaming</title>
+	<meta name='description' content='Gaming website designed by TareUhhhhhh'>
+	<meta property='og:image' content='https://peprojects.dev/alpha-3/tara/projects/tareuhhhhhh-gaming/images/skeleton.jpg'>
 
 	<link rel='stylesheet' href='css/style.css'>
 </head>
