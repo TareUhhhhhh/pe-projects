39c5ee6daab844061ec911ee06e40049cde238f6
Tara <tara.haskamp@gmail.com>
2022-01-20 19:45:17 -0600

    Remove unneccessary folder
    I copied the wrong project initially

--- projects/theme-challenge/cta.php
+++
@@ -1,9 +0,0 @@
-		<section class="cta">
-			<div class="inner-column">
-				<h1>This module is a "call to action</h1>
-
-				<p>These aren't names you should memorize. We're just making them up. Sometimes things get called something so many times that it sticks. This is a pretty common pattern.</p>
-
-				<a href="#" class="button">Here's the action!</a>
-			</div>
-		</section>
