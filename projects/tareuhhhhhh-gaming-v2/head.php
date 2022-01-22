5696c9102aa34af148de6a2494581a20e83a7599
Tara <tara.haskamp@gmail.com>
2022-01-21 19:04:25 -0600

    Create version 2 of TareUhhhhhh Gaming site

---
+++ projects/tareuhhhhhh-gaming-v2/head.php
@@ -0,0 +1,10 @@
+<head>
+	<meta charset='utf-8'>
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+
+	<title>TareUhhhhhh Gaming</title>
+	<meta name='description' content='Gaming website designed by TareUhhhhhh'>
+	<meta property='og:image' content='https://peprojects.dev/alpha-3/tara/projects/tareuhhhhhh-gaming/images/skeleton.jpg'>
+
+	<link rel='stylesheet' href='css/style.css'>
+</head>
