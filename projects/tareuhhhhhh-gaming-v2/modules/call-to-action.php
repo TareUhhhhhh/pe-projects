fc82810eb2038e3907368b66f6f160ed566d0e4e
Tara <tara.haskamp@gmail.com>
2022-01-22 00:33:13 -0600

    Update site text

--- projects/tareuhhhhhh-gaming-v2/modules/call-to-action.php
+++ projects/tareuhhhhhh-gaming-v2/modules/call-to-action.php
@@ -1,13 +1,10 @@
 
 <call-to-action>
-	<a class='back-to-top' href="#">Back to top</a>
-
-	<h2 class='attention-voice'>This module is a "call to action"</h2>
+	<h2 class='attention-voice'>Our Newest Release is </h2>
 
 	<p class='calm-voice'>These aren't names you should memorize. We're just making them up. Sometimes things get called something so many times, that it sticks. This is a pretty common pattern</p>
 
 	<a href='#'>
 		<span>Here's the action!</span>
 	</a>
-
 </call-to-action>
