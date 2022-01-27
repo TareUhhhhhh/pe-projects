a3adece30a7abda09b722c1210769868ee9e0856
Tara <tara.haskamp@gmail.com>
2022-01-27 14:05:44 -0600

    Style call to action background video

--- projects/tareuhhhhhh-gaming-v2/modules/call-to-action.php
+++ projects/tareuhhhhhh-gaming-v2/modules/call-to-action.php
@@ -1,5 +1,10 @@
 
 <call-to-action>
+	<video autoplay loop muted>
+		<source src="https://peprojects.dev/videos/dark.mp4"
+					  type="video/mp4">
+	</video>
+	
 	<h2 class='attention-voice'>Our Newest Release</h2>
 
 	<p class='calm-voice'>Action, adventure, drama, thrills, chills and scares! Not the latest Blumhouse film, but an all new game from TareUhhhhhh Gaming! It's called Spoops and Skills!</p>
@@ -7,9 +12,4 @@
 	<a href='#'>
 		<span>Dare to play?</span>
 	</a>
-
-	<video autoplay loop muted>
-		<source src="https://peprojects.dev/videos/dark.mp4"
-					  type="video/mp4">
-	</video>
 </call-to-action>
