c6b9c5b5481b59fc6d33c35951fc201ff04331bf
Tara <tara.haskamp@gmail.com>
2022-02-03 21:29:53 -0600

    Set up new formats and add default values

--- projects/php-aerobics/pizza-party.php
+++ projects/php-aerobics/pizza-party.php
@@ -1,13 +1,90 @@
 <!-- pseudocode
 
-Total Pizzas
-Total Slices = $slices * $pizza
+Initialize: 
+	# of people
+	# of pizzas
+	# of slices
 
-Need to round down # of slices
-Then figure out how many slices are left over
+Input: 
+	If input, get input
 
+Calculate:
+	# of total slices
+	Total slices / people -- round down (no fractions)
+	
+Generate Message:
+	How many pieces per person?
+	If leftovers, how many slices?
+
+Output:
+	Display messages
 
 // -->
+
+<?php
+	$people = "3";
+	$pizzas = "2";
+	$pizzaSlices = "8";
+	$submitted = isset($_POST["submitted"]);
+
+	if ($submitted) {
+		if(!empty($_POST["people"])) {
+			$people = $_POST["people"];
+			$people = floatval($people);
+		}
+
+		if(!empty($_POST["pizza"])) {
+			$pizza = $_POST["pizza"];
+			$pizza = floatval($pizza);
+		}
+
+		if(!empty($_POST["slices"])) {
+			$slices = $_POST["slices"];
+			$slices = floatval($slices);
+		}
+	}
+
+	$totalSlices = $pizzas * $pizzaSlices;
+	$slicesLeft = $totalSlices % $people;
+	$peopleSlices = $totalSlices - $slicesLeft;
+
+	$message = "There are $totalSlices total slices and each person will get $peopleSlices slices.";
+
+	function leftoverMessage($slices) {
+		if ($slices > 0) {
+			return "There will be $slices slices leftover.";
+		}
+	}
+?>
+
+<form method="POST">
+	<div>
+		<label>
+			How many people are coming?
+		</label>
+		<input type="number" name="people" value="<?=$people?>">
+	</div>
+
+	<div>
+		<label>
+			How many pizzas will you order?
+		</label>
+		<input type="number" name="pizza" value="<?=$pizzas?>">
+	</div>
+
+	<div>
+		<label>
+			How many slices per pizza?
+		</label>
+		<input type="number" name="slices" value="<?=$pizzaSlices?>">
+	</div>
+
+	<input type="submit" name="submitted">
+</form>
+
+<h1><?=$message?></h1>
+
+<?= leftoverMessage($slicesLeft) ?>
 
 <style>
 	p, label {
@@ -18,51 +95,3 @@ Then figure out how many slices are left over
 		margin: 10px;
 	}
 </style>
-
-<?php
-	if (isset($_POST["submitted"])) {
-		if(isset($_POST["people"])) {
-			$people = $_POST["people"];
-			$people = floatval($people);
-		}
-
-		if(isset($_POST["pizza"])) {
-			$pizza = $_POST["pizza"];
-			$pizza = floatval($pizza);
-		}
-
-		if(isset($_POST["slices"])) {
-			$slices = $_POST["slices"];
-			$slices = floatval($slices);
-		}
-
-		$totalSlices = $pizza * $slices / $people;
-
-		echo $totalSlices;
-	}
-?>
-
-<form method="POST">
-	<div>
-		<label>
-			How many people are coming?
-		</label>
-		<input type="number" name="people">
-	</div>
-
-	<div>
-		<label>
-			How many pizzas will you order?
-		</label>
-		<input type="number" name="pizza">
-	</div>
-
-	<div>
-		<label>
-			How many slices per pizza?
-		</label>
-		<input type="number" name="slices">
-	</div>
-
-	<input type="submit" name="submitted">
-</form>
