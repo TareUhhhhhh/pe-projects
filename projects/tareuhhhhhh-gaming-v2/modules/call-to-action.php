c627f5826c5bea7197f86de9d8acd3610ef92a11
Tara <tara.haskamp@gmail.com>
2022-02-11 22:40:41 -0600

    Set up PHP heading

--- projects/tareuhhhhhh-gaming-v2/modules/call-to-action.php
+++ projects/tareuhhhhhh-gaming-v2/modules/call-to-action.php
@@ -5,7 +5,7 @@
 					  type="video/mp4">
 	</video>
 	
-	<h2 class='attention-voice'>Our Newest Release</h2>
+	<h2 class='attention-voice'><?php echo $ctaHeading?></h2>
 
 	<p class='calm-voice'>Action, adventure, drama, thrills, chills and scares! Not the latest Blumhouse film, but an all new game from TareUhhhhhh Gaming! It's called Spoops and Skills!</p>
 
