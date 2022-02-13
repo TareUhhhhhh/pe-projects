c1ccbdcee0fd2e4d5313305df66a9d5cfd98a2a3
Tara <tara.haskamp@gmail.com>
2022-02-13 00:37:16 -0600

    Add goals page and styles

---
+++ projects/tareuhhhhhh/goals.php
@@ -0,0 +1,9 @@
+<?php include('header.php')?>
+
+<?php 
+$pageHeading = $pageData["goals"]["title"];
+$pageContent = $pageData["goals"]["content"];
+include('intro.php')?>
+
+
+<?php include('footer.php')?>
--- projects/tareuhhhhhh/page-data.php
+++ projects/tareuhhhhhh/page-data.php
@@ -7,12 +7,36 @@ $pageData = [
 
 				<p>This portfolio exists in order to showcase the work that I am currently working on or have completed. It is a constant work in progress and will be updated as more projects are completed. So stay tuned!</p>",
 	],
+
 	"writing" => [
 		"title" => "Writing",
 		"content" => "
 			<p>Currently learning all about the wonderful world of web development. Ultimately my goal is to obtain a job as a front end or back end web developer and improve my skillset while working with talented.</p>
 
 				<p>This portfolio exists in order to showcase the work that I am currently working on or have completed. It is a constant work in progress and will be updated as more projects are completed. So stay tuned!</p>",
+	],
+
+	"goals" => [
+		"title" => "GOALS!",
+		"content" => 
+			"<ul>
+				<li>Have a solid understanding of HTML
+					<ul>
+						<li>Be able to discuss the basics and understand how to read and write it.
+						</li>
+						<li>Be able to recognize and diagnose when there are issues with existing html.
+						</li>
+					</ul>
+				</li>
+				<li>
+					Have a solid 
+				</li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>",
 	],
 ];
 
