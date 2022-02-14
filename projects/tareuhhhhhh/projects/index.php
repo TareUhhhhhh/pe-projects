14cb0a93a24563a15b1f7f4ba20c1b678bf28f24
Tara <tara.haskamp@gmail.com>
2022-02-14 00:50:58 -0600

    Add projects page code

--- projects/tareuhhhhhh/projects/index.php
+++ projects/tareuhhhhhh/projects/index.php
@@ -1,1 +1,9 @@
-index.php
+<?php include('../header.php')?>
+
+<?php 
+$pageHeading = $pageData["projects"]["title"];
+$pageContent = $pageData["projects"]["content"];
+include('../intro.php')?>
+
+
+<?php include('../footer.php')?>
