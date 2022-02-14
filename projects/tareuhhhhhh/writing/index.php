473c75f701ac0a0a874f67fcbe8d47f5cefc93c5
Tara <tara.haskamp@gmail.com>
2022-02-14 00:53:06 -0600

    Add writing page PHP code

--- projects/tareuhhhhhh/writing/index.php
+++ projects/tareuhhhhhh/writing/index.php
@@ -1,1 +1,9 @@
-index.php
+<?php include('../header.php')?>
+
+<?php 
+$pageHeading = $pageData["writing"]["title"];
+$pageContent = $pageData["writing"]["content"];
+include('../intro.php')?>
+
+
+<?php include('../footer.php')?>
