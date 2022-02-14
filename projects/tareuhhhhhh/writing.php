3c023e36cc2f8bb7b592bbb40b7933a8b0a8ddb5
Tara <tara.haskamp@gmail.com>
2022-02-14 00:51:28 -0600

    Remove unneeded code and add footer

--- projects/tareuhhhhhh/writing.php
+++ projects/tareuhhhhhh/writing.php
@@ -5,15 +5,5 @@ $pageHeading = $pageData["writing"]["title"];
 $pageContent = $pageData["writing"]["content"];
 include('intro.php')?>
 
-<?php 
-$pageHeading = $pageData["about"]["title"];
-$pageContent = $pageData["about"]["content"];
-include('intro.php')?>
-
-<?php 
-$pageHeading = "Hello";
-$pageContent = $pageData["writing"]["content"];
-include('intro.php')?>
 
 <?php include('footer.php')?>
-
