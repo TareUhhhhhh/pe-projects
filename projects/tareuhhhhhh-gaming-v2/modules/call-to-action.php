fcd20c764ac5bb2b17b8ced8aaf971fcaed46955
Tara <tara.haskamp@gmail.com>
2022-02-15 18:08:02 -0600

    Remove video from CTA module

--- projects/tareuhhhhhh-gaming-v2/modules/call-to-action.php
+++ projects/tareuhhhhhh-gaming-v2/modules/call-to-action.php
@@ -1,8 +1,8 @@
 
 <call-to-action>
 	<video autoplay loop muted>
-		<source src="https://peprojects.dev/videos/dark.mp4"
-					  type="video/mp4">
+		<!-- <source src="https://peprojects.dev/videos/dark.mp4"
+					  type="video/mp4"> -->
 	</video>
 	
 	<h2 class='attention-voice'><?php echo $ctaHeading?></h2>
