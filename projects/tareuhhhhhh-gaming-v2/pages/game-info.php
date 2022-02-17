3d05f9647b7146b75d2061e6a3e39be85cd383d0
Tara <tara.haskamp@gmail.com>
2022-02-16 22:10:41 -0600

    Correct syntax

--- projects/tareuhhhhhh-gaming-v2/pages/game-info.php
+++ projects/tareuhhhhhh-gaming-v2/pages/game-info.php
@@ -10,8 +10,10 @@ $games = [
 			"Switch" => false,
 			"Xbox" => false,
 		],
-		"age-rating" => "M",
+		"ageRating" => "M",
 		"description" => "Six friends have to live their boring, everyday lives until after dusk. Who will survive?",
+		"longDescription" => "In a remote work environment, people are strewn across the world and forced tostare at everyone's bored faces and occassional fluffy friend. Amid the awful greenscreen backgrounds of beaches and highrise aparrtments, one of their co-workers has a secret. Will you be able to find out in time?",
+		"image" => "spooky.jpg",
 	],
 	[
 		"id" => "scare-layers",
@@ -22,7 +24,9 @@ $games = [
 			"Switch" => true,
 			"Xbox" => true,
 		],
-		"age-rating" => "M",
+		"ageRating" => "M",
 		"description" => "A dull artist can't paint a picture of his wife. Will you be able to prevent him from going crazy? (Doubt it)",
+		"longDescription" => "A boring artist wants to paint a picture of his wife. He has no talent and instead of admitting defeat he takes out his rage on everyone aound him. Can you sruvive his wrath?",
+		"image" => "creepy.jpg",
 	],
 ];
