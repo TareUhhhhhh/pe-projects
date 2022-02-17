9e8cd4de1d94bbc34eb8ca970ac6696c88716859
Tara <tara.haskamp@gmail.com>
2022-02-16 23:02:25 -0600

    Add comments for future reference

--- projects/tareuhhhhhh-gaming-v2/pages/game-detail.php
+++ projects/tareuhhhhhh-gaming-v2/pages/game-detail.php
@@ -1,12 +1,15 @@
 <?php
+	//Check the URL and see if there is a value for the key "game" -- if there is, we will route it
 	if (isset($_GET["game"])) {
 		$route = $_GET["game"];
 	}
 	
+	//Now we need to pull in the data file so we have the array information
 	include("game-info.php");
 
+	//Here we need to loop through all of the games to pull out the information.
 	foreach ($games as $game) {
-		if ($game["id"] == $route) {
+		if ($game["id"] == $route) { //Once the loop finds the id in the file that matches the route (from the URL) we will use that below - as the detail variable
 			$detail = $game;
 
 		}
