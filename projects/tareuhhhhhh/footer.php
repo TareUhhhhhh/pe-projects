d10f867331336d4068edefc77cea993238811a89
Tara <tara.haskamp@gmail.com>
2022-02-20 01:37:21 -0600

    Add flex and adjust font sizing on footer

--- projects/tareuhhhhhh/footer.php
+++ projects/tareuhhhhhh/footer.php
@@ -1,14 +1,16 @@
 		</main>
-		
+
 		<footer>
 			<div class="inner-column">
 				<h2>Contact</h2>
 
-				<p class="email">Feel free to <a href="mailto:tara.haskamp@gmail.com" target='email'>email me</a> to chat!</p>
+				<section class='contact'>
+					<p class="email">Feel free to <a href="mailto:tara@tareuhhhhhh.com" target='email'>email me</a> to chat!</p>
 
-				<p class="substack">You can also check out my writing that is posted to <a href="#" target='substack'>Substack</a>.</p>
+					<p class="substack">You can also check out my writing that is posted to <a href="#" target='substack'>Substack</a>.</p>
 
-				<p class="github">I also keep my projects on <a href="#" target='github'>Github</a> if you want to check them out!</p>
+					<p class="github">I also keep my projects on <a href="#" target='github'>Github</a> if you want to check them out!</p>	
+				</section>
 			</div>
 		</footer>
 	</body>
