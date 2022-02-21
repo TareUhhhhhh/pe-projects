3290bf1505fe278b65556a3ec9e26dc8474d87d1
Tara <tara.haskamp@gmail.com>
2022-02-20 21:41:46 -0600

    Add json file for project information

---
+++ projects/tareuhhhhhh/project-card.php
@@ -0,0 +1,27 @@
+<?php
+	
+	foreach($project["highlights"] as $highlights){
+	//decode each line to read as true?
+		//	if svg = true, echo svg
+		// if html = true, echo html
+	}
+
+?>
+
+<div class="inner-column">
+	<project-card>
+		<h2><?=$project["title"]?></h2>
+
+		<!-- <picture>
+			<img src="<?=$project["image"]?>">
+		</picture> -->
+
+		<!-- <h3><?=$project["highlights"]?></h3> -->
+
+		<p><?=$project["teaser"]?></p>
+
+		<p><?=$project["description"]?></p>
+
+		<a href="?project=<?=$project["slug"]?>">Link</a>
+	</project-card>
+</div>
