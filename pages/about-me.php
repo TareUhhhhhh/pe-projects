e889b47264220cdd7a61bfa79eda44f001b1b507
Tara <tara.haskamp@gmail.com>
2022-02-26 21:25:03 -0500

    Add about me information

--- pages/about-me.php
+++
@@ -1,9 +0,0 @@
-<?php include('header.php')?>
-
-<?php 
-$pageHeading = $pageData["about"]["title"];
-$pageContent = $pageData["about"]["content"];
-include('intro.php')?>
-
-
-<?php include('footer.php')?>
--- pages/about.php
+++ pages/about.php
@@ -1,3 +1,7 @@
 <?php
 
-	// echo "NEED TO FILL THIS IN!";
+//need this filled out, styled and finalized
+	// echo "I like to crochet, cross stitch, play video games, paint with watercolor and acrylics, among many other creative endeavors!";
+
+
+	//insert images - crochet, watercolor, cross-stitch, video games? cat, zoo?
