266216a5c9244d1a83ab3396231b2f3608ef6813
Tara <tara.haskamp@gmail.com>
2022-03-03 20:53:18 -0600

    Fix Substack and Github footer links

--- footer.php
+++ footer.php
@@ -7,9 +7,9 @@
 				<section class='contact'>
 					<p class="email">Feel free to <a href="mailto:tara@tareuhhhhhh.com" target='email'>email me</a> to chat!</p>
 
-					<p class="substack">You can also check out my writing that is posted to <a href="#" target='substack'>Substack</a>.</p>
+					<p class="substack">You can also check out my writing that is posted to <a href="https://tarahaskamp.substack.com/" target='substack'>Substack</a>.</p>
 
-					<p class="github">I also keep my projects on <a href="#" target='github'>Github</a> if you want to check them out!</p>	
+					<p class="github">I also keep my projects on <a href="https://github.com/TareUhhhhhh" target='github'>Github</a> if you want to check them out!</p>	
 				</section>
 			</div>
 		</footer>
