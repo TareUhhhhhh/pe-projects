cd0eee244ecd9ec31199b13af6e63f5e8c7610c6
Tara <tara.haskamp@gmail.com>
2022-03-29 18:57:59 -0500

    Adjust theme selector form styling

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
 			<div class="inner-column">
 				<nav class='info-nav'>
 					<a href="?">Home</a>
-					<!-- <a href="?page=projects">Projects</a> -->
+					<a href="?page=projects">Projects</a>
 					<a href="?page=writing">Writing</a>
 				</nav>
 
@@ -41,16 +41,18 @@
 					<!-- <a href="?page=fun">Fun</a> -->
 				</nav>
 
-				<form method="POST">
-					<label for='theme'>Theme</label>
-					<select name="theme" id="theme">
-						<option value="" disabled> -- Choose a theme --</option>
-						<option value="light" <?= ($theme == "light") ? "selected" : "" ?>>Light</option>
-						<option value="dark"<?= ($theme == "dark") ? "selected" : "" ?>>Dark</option>
-					</select>
+				<div class="theme-form">
+					<form method="POST" class='theme-select'>
+						<label for='theme'>Theme</label>
+						<select name="theme" id="theme">
+							<option value="" disabled> -- Choose a theme --</option>
+							<option value="light" <?= ($theme == "light") ? "selected" : "" ?>>Light</option>
+							<option value="dark"<?= ($theme == "dark") ? "selected" : "" ?>>Dark</option>
+						</select>
 
-					<button name='userOptedTheme'>Submit</button>
-				</form>
+						<button name='userOptedTheme'>Submit</button>
+					</form>
+				</div>
 			</div>
 		</header>
 
