6a63ca9e09c54b0c681a99a1408f4ef34c9797fd
Tara <tara.haskamp@gmail.com>
2022-04-07 21:28:58 -0500

    Update boilerplate code to php

---
+++ projects/boilerplate/components/content.php
@@ -0,0 +1,16 @@
+<section class="intro">
+<div class="inner-column">
+	<h1>H1 - Hello.</h1>
+
+	<p>This is the intro section. This will explain what this website is for.</p>
+</div>
+</section>
+
+
+<section class="more-info">
+<div class="inner-column">
+	<h2>H2 - More Info</h2>
+
+	<p>This is another section. Default it is "more-info" but can easily be changed to something else. Just update the class everywhere!</p>
+</div>
+</section>
