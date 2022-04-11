7293651e9507edb1dd21c20d229a4688853bb693
Tara <tara.haskamp@gmail.com>
2022-04-10 21:16:07 -0500

    Add more html elements

--- projects/substack-post-copy/components/content.php
+++ projects/substack-post-copy/components/content.php
@@ -24,12 +24,47 @@
 		<?php
 			include('modules/h1.php');
 			include('modules/paragraph.php');
-			include('modules/paragraph.php');
+		?>
+
+		<h3>
+			Heading Level Three
+		</h3>
+
+		<ul>
+			<li>UL Items</li>
+			<li>UL Items</li>
+			<li>UL Items</li>
+			<li>UL Items</li>
+			<li>UL Items</li>
+			<li>UL Items</li>
+			<li>UL Items</li>
+			<li>UL Items</li>
+		</ul>
+
+		<blockquote>
+			"This is a blockquote section. It should stand out from the rest of the page. It says special stuff."
+		</blockquote>
+
+		<?php
 			include('modules/h2.php');
 			include('modules/paragraph.php');
 			include('modules/picture.php');
 			include('modules/paragraph.php');
-			include('modules/paragraph.php');
+		?>
+
+		<h3>
+			Heading Level Three
+		</h3>
+
+		<ol>
+			<li>OL Items</li>
+			<li>OL Items</li>
+			<li>OL Items</li>
+			<li>OL Items</li>
+			<li>OL Items</li>
+		</ol>
+
+		<?php
 			include('modules/picture.php');
 			include('modules/paragraph.php');
 			include('modules/paragraph.php');
---
+++ projects/substack-post-copy/modules/h3.php
@@ -0,0 +1,3 @@
+<h3>
+	Heading Level Three
+</h3>
