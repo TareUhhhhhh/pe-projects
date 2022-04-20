3483a450c0132b2f61f7601f9984ec587eb7a9f3
Tara <tara.haskamp@gmail.com>
2022-04-19 20:37:11 -0500

    Add new responsive format for card gallery

--- projects/zen-responsive-garden/modules/card-gallery.php
+++ projects/zen-responsive-garden/modules/card-gallery.php
@@ -6,26 +6,38 @@
 	<div class="inner-column">
 		<card>
 			<picture><img src="https://peprojects.dev/images/square.jpg" alt=""></picture>
-			<h3>Card Title</h3>
-			<p>This is a description of the card for this particular item.</p>
+			<div class="card-content">
+				<h3>Card Title</h3>
+
+				<p>This is a description of the card for this particular item.</p>
+			</div>
 		</card>
 
 		<card>
 			<picture><img src="https://peprojects.dev/images/square.jpg" alt=""></picture>
-			<h3>Card Title</h3>
-			<p>This is a description of the card for this particular item.</p>
+			<div class="card-content">
+				<h3>Card Title</h3>
+
+				<p>This is a description of the card for this particular item.</p>
+			</div>
 		</card>
 
 		<card>
 			<picture><img src="https://peprojects.dev/images/square.jpg" alt=""></picture>
-			<h3>Card Title</h3>
-			<p>This is a description of the card for this particular item.</p>
+			<div class="card-content">
+				<h3>Card Title</h3>
+
+				<p>This is a description of the card for this particular item.</p>
+			</div>
 		</card>
 
 		<card>
 			<picture><img src="https://peprojects.dev/images/square.jpg" alt=""></picture>
-			<h3>Card Title</h3>
-			<p>This is a description of the card for this particular item.</p>
+			<div class="card-content">
+				<h3>Card Title</h3>
+				
+				<p>This is a description of the card for this particular item.</p>
+			</div>
 		</card>
 	</div>
 </section>
