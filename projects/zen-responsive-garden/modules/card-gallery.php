c67b12e812fda898ca674705630f3cff1507321d
Tara <tara.haskamp@gmail.com>
2022-04-19 21:49:37 -0500

    Fix card image max width

--- projects/zen-responsive-garden/modules/card-gallery.php
+++ projects/zen-responsive-garden/modules/card-gallery.php
@@ -5,38 +5,46 @@
 <section class="card-gallery">
 	<div class="inner-column">
 		<card>
-			<picture><img src="https://peprojects.dev/images/square.jpg" alt=""></picture>
+			<picture><img src="https://picsum.photos/1000/800?random=1" alt="random image"></picture>
 			<div class="card-content">
 				<h3>Card Title</h3>
 
 				<p>This is a description of the card for this particular item.</p>
+
+				<a href="x">Clink the Link!</a>
 			</div>
 		</card>
 
 		<card>
-			<picture><img src="https://peprojects.dev/images/square.jpg" alt=""></picture>
+			<picture><img src="https://picsum.photos/1000/800?random=2" alt="random image"></picture>
 			<div class="card-content">
 				<h3>Card Title</h3>
 
 				<p>This is a description of the card for this particular item.</p>
+
+				<a href="x">Clink the Link!</a>
 			</div>
 		</card>
 
 		<card>
-			<picture><img src="https://peprojects.dev/images/square.jpg" alt=""></picture>
+			<picture><img src="https://picsum.photos/1000/800?random=3" alt="random image"></picture>
 			<div class="card-content">
 				<h3>Card Title</h3>
 
 				<p>This is a description of the card for this particular item.</p>
+
+				<a href="x">Clink the Link!</a>
 			</div>
 		</card>
 
 		<card>
-			<picture><img src="https://peprojects.dev/images/square.jpg" alt=""></picture>
+			<picture><img src="https://picsum.photos/1000/800?random=4" alt="random image"></picture>
 			<div class="card-content">
 				<h3>Card Title</h3>
 				
 				<p>This is a description of the card for this particular item.</p>
+
+				<a href="x">Clink the Link!</a>
 			</div>
 		</card>
 	</div>
