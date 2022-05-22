5aacaa181ee4b17b80101a3216dc3ab2d1495d84
Tara <tara.haskamp@gmail.com>
2022-05-21 20:45:32 -0500

    Move theme selector

--- footer.php
+++ footer.php
@@ -9,6 +9,19 @@
 
 				<p class="github">I also keep my projects on <a href="https://github.com/TareUhhhhhh" target='github'>Github</a> if you want to check them out!</p>	
 			</section>
+			
+			<div class="theme-form">
+					<form method="POST" class='theme-select'>
+						<label for='theme'>Theme</label>
+						<select name="theme" id="theme">
+							<option value="" disabled> -- Choose a theme --</option>
+							<option value="light" <?= ($theme == "light") ? "selected" : "" ?>>Light</option>
+							<option value="dark"<?= ($theme == "dark") ? "selected" : "" ?>>Dark</option>
+						</select>
+
+						<button name='userOptedTheme'>Submit</button>
+					</form>
+				</div>
 		</div>
 		</footer>
 	</body>
--- header.php
+++ header.php
@@ -38,19 +38,6 @@
 					<a href="?page=about">About</a>
 					<!-- <a href="?page=fun">Fun</a> -->
 				</nav>
-
-				<div class="theme-form">
-					<form method="POST" class='theme-select'>
-						<label for='theme'>Theme</label>
-						<select name="theme" id="theme">
-							<option value="" disabled> -- Choose a theme --</option>
-							<option value="light" <?= ($theme == "light") ? "selected" : "" ?>>Light</option>
-							<option value="dark"<?= ($theme == "dark") ? "selected" : "" ?>>Dark</option>
-						</select>
-
-						<button name='userOptedTheme'>Submit</button>
-					</form>
-				</div>
 			</div>
 		</header>
 
