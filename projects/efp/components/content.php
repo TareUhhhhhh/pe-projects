907ecb2c435f42dba4933aa956165ac468e4baf4
Tara <tara.haskamp@gmail.com>
2022-06-07 23:42:35 -0500

    Add links to completed forms

--- projects/efp/components/content.php
+++ projects/efp/components/content.php
@@ -11,8 +11,12 @@
 <div class="inner-column">
 	<h2 class='more-info'>The Forms</h2>
 
-	<p class='more-info'>Here they are.</p>
+		<a href="forms/001-hello-world.php" target='hello'>Hello World</a>
+		<a href="forms/002-counting-characters.php" target='count'>Counting Characters</a>
+		<a href="forms/003-printing-quotes.php" target='quotes'>Printing Quotes</a>
+		<a href="forms/004-mad-lib.php" target='mad'>Mad Lib</a>
 
 	<!-- Add foreach loop in the future for files? -->
+
 </div>
 </section>
