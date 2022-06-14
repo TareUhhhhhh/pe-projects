39f9e82dd0fc3664bce1851e2d0c35d16d3d8059
Tara <tara.haskamp@gmail.com>
2022-06-13 23:15:10 -0500

    Update meta data

--- projects/mk-ultra-responsive/components/head.php
+++ projects/mk-ultra-responsive/components/head.php
@@ -4,9 +4,9 @@
 	<head>
 		<meta charset='utf-8'>
 		<meta name='viewport' content='width=device-width, initial-scale=1'>
-		<title>Base</title>
-		<meta name='description' content='[[insert description]]'>
-		<meta property='og:image' content='images/default.png'>
+		<title>MK ULTRA...responsive</title>
+		<meta name='description' content="So responsive...you can't even know about it">
+		<meta property='og:image' content='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy3SBvSg88kKxyUtnpX_u6LCG9DyZKQRbVwZH659-O1qHeP7owFZmTMO5EAW3Uo6UBBxk&usqp=CAU'>
 		<link rel='stylesheet' href='css/style.css'>
 	</head>
 
