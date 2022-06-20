ef348289a27a3f71b8c547907d2aba64c4a9c12d
Tara <tara.haskamp@gmail.com>
2022-06-19 20:53:06 -0500

    Add images

--- projects/theme-challenge/data/articles.php
+++ projects/theme-challenge/data/articles.php
@@ -4,31 +4,31 @@ $articles = [
 	[
 		"heading" => "Article or product",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "landscape.jpg",
+		"thumbnail" => "https://www.mymove.com/wp-content/uploads/2014/05/GettyImages-147205632.jpg",
 	],
 	[
 		"heading" => "Destination or option",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "landscape.jpg",
+		"thumbnail" => "https://photos.zillowstatic.com/fp/edd8a99b6634838e81c7651a78889522-p_e.jpg",
 	],
 	[
 		"heading" => "Story or something",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "landscape.jpg",
+		"thumbnail" => "https://photos.zillowstatic.com/fp/17d6d46bceba8968338b8443119f8dc9-p_e.jpg",
 	],
 	[
 		"heading" => "Banana tandori",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "landscape.jpg",
+		"thumbnail" => "https://mir-s3-cdn-cf.behance.net/project_modules/1400/70983074973155.5c3f7fc151921.jpg",
 	],
 	[
 		"heading" => "Bar B Q",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "landscape.jpg",
+		"thumbnail" => "https://rew-feed-images.global.ssl.fastly.net/hmls/_cloud_media/property/residential/hms2381058-1-cf74531e19887e67547d9f682292a4e0-m.jpg",
 	],
 	[
 		"heading" => "Marsala",
 		"description" => "Here's a little info to help you understand if this is a thing you want to know about.",
-		"thumbnail" => "landscape.jpg",
+		"thumbnail" => "https://i.etsystatic.com/21180532/r/il/418850/2933808655/il_570xN.2933808655_1qqx.jpg",
 	],
 ];
