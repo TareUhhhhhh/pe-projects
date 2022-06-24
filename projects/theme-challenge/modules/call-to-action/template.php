0708c3d17270e975713bf147505835b57046d03d
Tara <tara.haskamp@gmail.com>
2022-06-23 19:30:49 -0500

    Add image to call to action module

--- projects/theme-challenge/modules/call-to-action/template.php
+++ projects/theme-challenge/modules/call-to-action/template.php
@@ -1,8 +1,12 @@
 
 <call-to-action>
-	<h2 class='heading attention-voice'>When you think luxury, think of Luxury Homes</h2>
+	<picture class='cta'><img src="https://img.gtsstatic.net/reno/imagereader.aspx?imageurl=http%3A%2F%2Fm.sothebysrealty.com%2F4i0%2Fw4z16xrbwerbmseatha9ktw055i&option=N&h=375&permitphotoenlargement=false" alt=""></picture>
 
-	<p class='story'>Luxury Homes can hep you find the perfect home in the perfect place.</p>
+	<text-content class='cta'>
+		<h2 class='heading attention-voice'>Connect With a Luxury Homes Agent</h2>
 
-	<a class='button' href='#'>Find Homes Now</a>
+		<p class='story'>Luxury Homes has agents all across the world that can hep you find the perfect home in the perfect place.</p>
+
+		<a class='cta calm-voice' href='#'>Find Homes Now</a>
+	</text-content>
 </call-to-action>
