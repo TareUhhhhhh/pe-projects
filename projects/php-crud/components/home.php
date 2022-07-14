de2466431220889a56b5299290ddc6b084673307
Tara <tara.haskamp@gmail.com>
2022-07-13 21:18:38 -0500

    Add home page intro

--- projects/php-crud/components/home.php
+++ projects/php-crud/components/home.php
@@ -1,1 +1,12 @@
 <!-- Need to add home page stuff here -->
+<section class="home-intro">
+	<h1>Artfully</h1>
+
+	<h2>A Collection of Creative Works</h2>
+
+	<p>This site is designed as a collection to share works of art with the world. It was started as a place for me to showcase my crafts as well as my son's drawings and art.</p>
+
+	<p>It has since grown to be a collective of artists from all over sharing their pieces as well.</p>
+
+	<p>So feel free to look around, check out what is here and feel free to submit your own!</p>
+</section>
