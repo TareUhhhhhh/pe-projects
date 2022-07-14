7350e246fb1b70ecf6036a4a87e95ab77f08d088
Tara <tara.haskamp@gmail.com>
2022-07-13 21:16:55 -0500

    Correct Site meta data

--- head.php
+++ head.php
@@ -1,7 +1,7 @@
 
 <meta charset='utf-8'>
 <meta name='viewport' content='width=device-width, initial-scale=1'>
-<title>TareUhhhhhh v2</title>
+<title>TareUhhhhhh</title>
 <meta name='description' content='The site of TareUhhhhhh'>
 <meta property='og:image' content='images/skull.jpg'>
 <link rel='stylesheet' href='css/style.css'>
