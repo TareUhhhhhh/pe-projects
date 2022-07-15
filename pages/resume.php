54a9d4ff93483396f097cc7ada3260dc9877aecc
Tara <tara.haskamp@gmail.com>
2022-07-14 22:06:51 -0500

    Simplify resume job styling

--- pages/resume.php
+++ pages/resume.php
@@ -1,6 +1,7 @@
 
 <section class="resume">
 <div class="inner-column">
+
 <!-- Get resume info from json file
 Job info
 Education Info -->
@@ -12,19 +13,19 @@ Education Info -->
 
 	<article class="resume-entry">
 		<div class="company">
-			<h2 class="company">Company: </h2>
+			<!-- <h2 class="company">Company: </h2> -->
 
-			<h3 class='company'><?=$entry['company']?></h3>
+			<h2 class='company'><?=$entry['company']?></h2>
 		</div>
 
 		<div class="job-title">
-			<h2 class="job-title">Job Title: </h2>
+			<!-- <h2 class="job-title">Job Title: </h2> -->
 
 			<h3 class="job-title"><?=$entry['title']?></h3>
 		</div>
 
 		<div class="years-active">
-			<h2 class="years-active">Years: </h2>
+			<!-- <h2 class="years-active">Years: </h2> -->
 
 			<h3 class="years-active"><?=$entry['years']?></h3>
 		</div>
