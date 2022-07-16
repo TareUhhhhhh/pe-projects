febc451f570a87b6b905571b3f8122d5cadebe17
Tara <tara.haskamp@gmail.com>
2022-07-16 00:38:53 -0500

    Rearrange PHP to display work and school separate

--- pages/resume.php
+++ pages/resume.php
@@ -1,45 +1,70 @@
 <page-content class="resume">
 <div class="inner-column">
 
+<section class="work-history">
+	<h2 class='section-title'>Work History</h2>
+
 <?php
 	$json = file_get_contents('data/resumeData.json');
 	$resumeData = json_decode($json, true);
 
 	foreach ($resumeData as $entry) { 
 
-		if ( $entry['type'] == "job" ) { ?>
+		if ( $entry['type'] == "job" ) { 
+			$company = $entry['company'];
+			$jobTitle = $entry['title'];
+			$yearsActive = $entry['years'];
+	?>
 
-	<article class="resume-entry job">
+		<article class="resume-entry job">
 		<div class="company">
+			<h2 class='company'><?=$company?></h2>
 		</div>
 
 		<div class="job-title">
-
-			<h3 class="job-title"><?=$entry['title']?></h3>
+			<h3 class="job-title"><?=$jobTitle?></h3>
 		</div>
 
 		<div class="years-active">
+			<h3 class="years-active"><?=$yearsActive?></h3>
+		</div>
 
-			<h3 class="years-active"><?=$entry['years']?></h3>
+		<div class="responsibilities">
+			<ul class="responsiblities">
+				<?php
+					foreach( $entry['responsibilities'] as $duty) { ?>
+
+						<li><?=$duty?></li>
+
+				<?php	} ?>
+			</ul>
 		</div>
 	</article>
 
-<?php	} 
-	if ($entry['type'] == "school") { ?>
+	<?php	} } ?>
+
+	</section>
 	
-	<article class="resume-entry school">
-		<div class="school-name">
-			<h2 class="school-name"><?=$entry['name']?></h2>
-		</div>
+	<section class="education">
+		<h2 class="section-title">Education</h2>
+	<?php
+		foreach ($resumeData as $entry) { 
+			if ($entry['type'] == "school") {
+				$school = $entry['name'];
+				$yearsAttended = $entry['years'];
+	?>
+		<article class="resume-entry school">
+			<div class="school-name">
+				<h2 class="school-name"><?=$entry['name']?></h2>
+			</div>
 
-		<div class="years-attended">
-			<h3 class="years"><?=$entry['years']?></h3>
-	</article>
-		</div>
-
-<?php	}
-
-} ?>
+			<div class="years-attended">
+				<h3 class="years"><?=$entry['years']?></h3>
+			</div>
+		</article>
+	<?php		}
+	} ?>
+	</section>
 
 </div>
 </page-content>
