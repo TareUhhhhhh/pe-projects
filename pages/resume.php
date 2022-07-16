22a0238fc7fedd778d81373f5db90fa9016ef980
Tara <tara.haskamp@gmail.com>
2022-07-16 00:38:07 -0500

    Remove unused code

--- pages/resume.php
+++ pages/resume.php
@@ -1,9 +1,6 @@
 <page-content class="resume">
 <div class="inner-column">
 
-<!-- Get resume info from json file
-Job info
-Education Info -->
 <?php
 	$json = file_get_contents('data/resumeData.json');
 	$resumeData = json_decode($json, true);
@@ -14,19 +11,14 @@ Education Info -->
 
 	<article class="resume-entry job">
 		<div class="company">
-			<!-- <h2 class="company">Company: </h2> -->
-
-			<h2 class='company'><?=$entry['company']?></h2>
 		</div>
 
 		<div class="job-title">
-			<!-- <h2 class="job-title">Job Title: </h2> -->
 
 			<h3 class="job-title"><?=$entry['title']?></h3>
 		</div>
 
 		<div class="years-active">
-			<!-- <h2 class="years-active">Years: </h2> -->
 
 			<h3 class="years-active"><?=$entry['years']?></h3>
 		</div>
