68a62694c003fa94e2d9e5a40bbe0d8f91589805
Tara <tara.haskamp@gmail.com>
2022-07-18 22:59:27 -0500

    Update resume sections

--- pages/resume.php
+++ pages/resume.php
@@ -1,69 +1,37 @@
 <page-content class="resume">
 <div class="inner-column">
 
-<section class="work-history">
-	<h2 class='section-title'>Work History</h2>
+	<section class="work-history">
+		<h2 class='section-title'>Work History</h2>
 
-<?php
-	$json = file_get_contents('data/resumeData.json');
-	$resumeData = json_decode($json, true);
+		<?php
+		$json = file_get_contents('data/resumeData.json');
+		$resumeData = json_decode($json, true);
 
-	foreach ($resumeData as $entry) { 
+		foreach ($resumeData as $entry) { 
 
-		if ( $entry['type'] == "job" ) { 
-			$company = $entry['company'];
-			$jobTitle = $entry['title'];
-			$yearsActive = $entry['years'];
-	?>
+			if ( $entry['type'] == "job" ) { 
+				$company = $entry['company'];
+				$jobTitle = $entry['title'];
+				$yearsActive = $entry['years'];
 
-		<article class="resume-entry job">
-		<div class="company">
-			<h2 class='company'><?=$company?></h2>
-		</div>
-
-		<div class="job-title">
-			<h3 class="job-title"><?=$jobTitle?></h3>
-		</div>
-
-		<div class="years-active">
-			<h3 class="years-active"><?=$yearsActive?></h3>
-		</div>
-
-		<div class="responsibilities">
-			<ul class="responsiblities">
-				<?php
-					foreach( $entry['responsibilities'] as $duty) { ?>
-
-						<li><?=$duty?></li>
-
-				<?php	} ?>
-			</ul>
-		</div>
-	</article>
-
-	<?php	} } ?>
+				include('cards/job-card.php');
+			} 
+		} ?>
 
 	</section>
 	
 	<section class="education">
 		<h2 class="section-title">Education</h2>
-	<?php
-		foreach ($resumeData as $entry) { 
-			if ($entry['type'] == "school") {
-				$school = $entry['name'];
-				$yearsAttended = $entry['years'];
-	?>
-		<article class="resume-entry school">
-			<div class="school-name">
-				<h2 class="school-name"><?=$entry['name']?></h2>
-			</div>
+		<?php
+			foreach ($resumeData as $entry) { 
+				if ($entry['type'] == "school") {
+					$school = $entry['name'];
+					$yearsAttended = $entry['years'];
 
-			<div class="years-attended">
-				<h3 class="years"><?=$entry['years']?></h3>
-			</div>
-		</article>
-	<?php		}
-	} ?>
+					include('cards/school-card.php');
+				}
+		} ?>
 	</section>
 
 </div>
