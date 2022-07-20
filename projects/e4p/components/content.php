7731e8f3aa701453a7102cbd1173b0b287681077
Tara <tara.haskamp@gmail.com>
2022-07-20 10:23:16 -0500

    Add numbers to index links

--- projects/e4p/components/content.php
+++ projects/e4p/components/content.php
@@ -11,14 +11,16 @@
 <div class="inner-column">
 	<h2 class='forms'>The Forms</h2>
 
-		<a href="forms/001-hello-world.php" target='hello'>Hello World</a>
-		<a href="forms/002-counting-characters.php" target='count'>Counting Characters</a>
-		<a href="forms/003-printing-quotes.php" target='quotes'>Printing Quotes</a>
-		<a href="forms/004-mad-lib.php" target='mad'>Mad Lib</a>
-		<a href="forms/005-simple-math.php" target='math'>Simple Math</a>
-		<a href="forms/006-retirement-calculator.php" target='retire'>Retirement Calculator</a>
-		<a href="forms/007-rectangular-area.php" target='retire'>Rectangular Area</a>
-		<a href="forms/010-self-checkout.php" target='retire'>Self Checkout</a>
+		<a href="forms/001-hello-world.php" target='hello'>1. Hello World</a>
+		<a href="forms/002-counting-characters.php" target='count'>2. Counting Characters</a>
+		<a href="forms/003-printing-quotes.php" target='quotes'>3. Printing Quotes</a>
+		<a href="forms/004-mad-lib.php" target='mad'>4. Mad Lib</a>
+		<a href="forms/005-simple-math.php" target='math'>5. Simple Math</a>
+		<a href="forms/006-retirement-calculator.php" target='retire'>6. Retirement Calculator</a>
+		<a href="forms/007-rectangular-area.php" target='rectangle'>7. Rectangular Area</a>
+		<a href="forms/008-pizza-party.php" target='pizza'>8. Pizza Party</a>
+		<a href="forms/009-paint-calculator.php" target='paint'>9. Paint Calculator</a>
+		<a href="forms/010-self-checkout.php" target='retire'>10. Self Checkout</a>
 
 	<!-- Add foreach loop in the future for files? -->
 
