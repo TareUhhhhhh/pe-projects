18f67f043f63125eec378ce25be95f4aae8fe0c3
Tara <tara.haskamp@gmail.com>
2022-08-03 23:33:24 -0500

    Update section code and add comments

--- pages/project.php
+++ pages/project.php
@@ -5,18 +5,18 @@
 <section>
 	<div class="inner-column">
 		<h1><?=$project["title"]?></h1>
-
-		<h2>Case Study</h2>
 	</div>
 </section>
 
 <section>
 	<div class="inner-column">
 		<?php
+
+		//This refers to any sort of repeatable kind of content which can be displayed in the prjectData json file -- need to revisit to figure out how to set this up
 			if(isset($project["sections"])) {
 				foreach($project["sections"] as $section) {
-					echo $section["type"];
-				};
+					$type = $section["type"]; 
+					};
 			}
 		?>
 	</div>
