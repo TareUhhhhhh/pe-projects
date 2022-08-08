078c6f084995fe4f5d77b8797a66ab71c5671c42
Tara <tara.haskamp@gmail.com>
2022-08-08 18:30:43 -0500

    Update link targets and add github link

--- pages/project.php
+++ pages/project.php
@@ -2,25 +2,25 @@
 	$project = getRecordBySlug($_GET["project"]);
 ?>
 
-<section>
 	<div class="inner-column">
 		<h2><?=$project["title"]?></h2>
 	</div>
-</section>
 
-<section>
+<section class='project-info'>
 	<div class="inner-column">
-		<?php
-		<a class='project-link' target='<?=$project['slug']?>' href="../<?=$project["link"]?>">Live Site</a>
+		<a class='project-link' target='<?=$project['title']?>' href="../<?=$project["link"]?>">Live Site</a>
+		<a class='github-link' target="<?=$project['gitLink']?>" href="https://github.com/TareUhhhhhh/pe-projects/tree/master/projects/<?=$project['slug']?>">GitHub Link</a>
 
+		<?php
 		//This refers to any sort of repeatable kind of content which can be displayed in the prjectData json file -- need to revisit to figure out how to set this up
 			if(isset($project["sections"])) {
 				foreach($project["sections"] as $section) {
-					$type = $section["type"]; 
+					$type = $section["type"];
 
-					if ($type == "highlights") { 
-						include('highlights.php');
-						}
+					if ($type == "highlights") {
+						include('sections/highlights.php');
+					}
+					// renderSection($type);
 					};
 			}
 		?>
