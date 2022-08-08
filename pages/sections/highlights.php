011ad6c11ac0cd325b1d05a4a475d6c8a5d6048f
Tara <tara.haskamp@gmail.com>
2022-08-08 10:07:56 -0500

    Move highlights php file into sections folder

--- pages/sections/highlights.php
+++ pages/sections/highlights.php
@@ -2,9 +2,12 @@
 	<h3>Project Highlights:</h3>
 	<ul class='highlights-list'>
 		<?php
+			if ($section['type'] == 'highlights') {
 			foreach ($section["highlights"] as $highlight) { ?>
+
 				<li class='highlight-item'><?=$highlight?></li>
-		<?php	}
+
+		<?php	} }
 		?>
 	</ul>
 </section>
