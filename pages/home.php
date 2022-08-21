bf024368258f7130340bc4dc6919890c806a69ff
Tara <tara.haskamp@gmail.com>
2022-08-20 20:02:56 -0500

    Create home file to include about

---
+++ pages/home.php
@@ -0,0 +1,10 @@
+<?php
+	//get json file data
+	$file = file_get_contents("pages/about-images.json");
+	$images = json_decode($file, true);
+	//foreach loop to display images 
+?>
+
+<?php 
+	include('about.php');
+?>
