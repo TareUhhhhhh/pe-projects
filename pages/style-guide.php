8fd73a711352c6012484cb24927d18cd005dc3ee
Tara <tara.haskamp@gmail.com>
2022-08-23 22:32:38 -0500

    Add modules to style guide file

--- pages/style-guide.php
+++ pages/style-guide.php
@@ -1,3 +1,10 @@
-<?php
-	include('projects.php');
-?>
+<section class="content">
+<div class="inner-column">
+	<?php
+		include('modules/page-intro.php');
+		include('modules/project-grid.php');
+		include('pages/modules/highlights.php');
+		include('screenshot.php');
+	?>
+</div>
+</section>
