9cc835d702ace5c092cca512d4cb9661d347d83a
Tara <tara.haskamp@gmail.com>
2022-08-24 21:11:15 -0500

    Add style guide project data

--- pages/style-guide.php
+++ pages/style-guide.php
@@ -1,10 +1,16 @@
+<?php
+	$projectsJson = file_get_contents('data/projectData.json');
+	$projects = json_decode($projectsJson, true);
+?>
+
 <section class="content">
 <div class="inner-column">
 	<?php
-		include('modules/page-intro.php');
-		include('modules/project-grid.php');
-		include('pages/modules/highlights.php');
-		include('screenshot.php');
+		if(isset($project["modules"])) {
+			foreach($project["modules"] as $module) {
+				renderModule($module);
+			};
+		}
 	?>
 </div>
 </section>
