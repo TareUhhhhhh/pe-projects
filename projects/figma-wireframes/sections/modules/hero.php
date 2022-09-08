d5e4901ced786e10164574e9a09b6cc54c4e4d41
Tara <tara.haskamp@gmail.com>
2022-09-07 21:33:45 -0500

    Add button links to hero module

--- projects/figma-wireframes/sections/modules/hero.php
+++ projects/figma-wireframes/sections/modules/hero.php
@@ -4,4 +4,10 @@
 
 		<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error ullam quis, deserunt laboriosam provident porro pariatur nisi commodi facilis.</p>
 	</div>
+
+	<div class="links">
+		<a href="#" class='button'>Button Link</a>
+
+		<a href="#" class='button'>Button Link</a>
+	</div>
 </hero-module>
