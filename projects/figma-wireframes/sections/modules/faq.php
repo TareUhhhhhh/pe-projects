c352af018d9822bb7e070ca0126046e6f534064d
Tara <tara.haskamp@gmail.com>
2022-09-09 09:45:11 -0500

    Add FAQ and footer html

--- projects/figma-wireframes/sections/modules/faq.php
+++ projects/figma-wireframes/sections/modules/faq.php
@@ -1,1 +1,49 @@
-faq.php
+<faq-section>
+	<div class="section-title">
+		<h1>Frequently Asked Questions</h1>
+
+		<p>Pellentesque cras adipiscing tempus libero vel nullam mauris tellus. Aliquam ultrices tellus consequat amet, lectus aliquam est in neque.</p>
+	</div>
+
+	<div class="question-module">
+		<details closed>
+			<summary>
+				<h3>How soon will my order ship?</h3>
+			</summary>
+
+			<p>It's a surprise!</p>
+		</details>
+
+		<details open>
+			<summary>
+				<h3>Are there any warranty exclusions?</h3>
+			</summary>
+
+			<p>Probably!</p>
+		</details>
+
+		<details closed>
+			<summary>
+				<h3>Are there any return exclusions?</h3>
+			</summary>
+
+			<p>Absolutely</p>
+		</details>
+
+		<details closed>
+			<summary>
+				<h3>Will I be charged for a replacement item?</h3>
+			</summary>
+
+			<p>Well, duh...</p>
+		</details>
+
+		<details closed>
+			<summary>
+				<h3>How can I track my orders & payment?</h3>
+			</summary>
+
+			<p>I would recommend a checkbook or online banking.</p>
+		</details>
+	</div>
+</faq-section>
--- projects/figma-wireframes/sections/modules/footer.php
+++ projects/figma-wireframes/sections/modules/footer.php
@@ -1,1 +1,63 @@
-footer.php
+<footer>
+	<footer-nav>
+		<div class="product-nav">
+			<h4>PRoducts</h4>
+
+			<ul>
+				<li><a href="#"></a>Diam orci</li>
+				<li><a href="#"></a>Mi feigiat</li>
+				<li><a href="#"></a>Netus fermentum</li>
+				<li><a href="#"></a>Suspendisse viverra</li>
+				<li><a href="#"></a>Id dolor</li>
+				<li><a href="#"></a>Erat mattis</li>
+			</ul>
+		</div>
+
+		<div class="info-nav">
+			<h4>Information</h4>
+
+			<ul>
+				<li><a href="#"></a>Id maecenas</li>
+				<li><a href="#"></a>Id orci</li>
+				<li><a href="#"></a>Magna ultricies</li>
+			</ul>
+		</div>
+
+		<div class="company-nav">
+			<h4>Company</h4>
+
+			<ul>
+				<li><a href="#"></a>Id maecenas</li>
+				<li><a href="#"></a>Id orci</li>
+				<li><a href="#"></a>Magna ultricies</li>
+				<li><a href="#"></a>Quis risus</li>
+			</ul>
+		</div>
+	</footer-nav>
+
+	<form action="_POST" class="subscribe">
+		<label><h3>Subscribe</h3></label>
+
+		<input type="email">
+
+		<input type="button">
+
+		<p>Gravida sed justo, justo, id est et. Amet tristique convallis sed porttitor nullam eu ut. Duis et odio aliquam bibendum. Metus et lectus id viverra fringilla magna morbi. </p>
+	</form>
+
+	<div class="legal-nav">
+		<picture class="logo">
+			<?php include('sections/components/logo.svg'); ?>
+		</picture>
+
+		<ul class="legal-links">
+			<li><a href="#">Terms</a></li>
+			<li><a href="#">Privacy</a></li>
+			<li><a href="#">Cookies</a></li>
+		</ul>
+
+		<div class="social-links">
+			<?php include('sections/components/social.svg'); ?>
+		</div>
+	</div>
+</footer>
