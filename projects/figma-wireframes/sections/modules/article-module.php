b519130fc3476f889348675c312899b54190cf0a
Tara <tara.haskamp@gmail.com>
2022-09-09 22:54:20 -0500

    Add call to sction and article module styling

--- projects/figma-wireframes/sections/modules/article-module.php
+++ projects/figma-wireframes/sections/modules/article-module.php
@@ -1,34 +1,77 @@
 <article-module>
-	<div class="title">
-		<h2>This is the article module title area</h2>
+	<article-display>
+		<div class="title">
+			<h2>This is the article module title area</h2>
 
-		<p>This is the subtitle area of the article module</p>
-	</div>
+			<p>This is the subtitle area of the article module</p>
+		</div>
 
-	<picture>
-		<img src="https://peprojects.dev/images/square.jpg" alt="">
-	</picture>
+		<picture class='article'>
+			<img src="https://peprojects.dev/images/square.jpg" alt="">
+		</picture>
+	</article-display>
 
 	<ol class="article-list">
 		<li>
-			<h3>Article Number One</h3>
-			<p>This is a little blurb about the article to let you decide if you want to click</p>
+			<div class="article-number">
+				1
+			</div>
+
+			<div class="article-teaser">
+				<h3>Article Number One</h3>
+
+				<p>This is a little blurb about the article to let you decide if you want to click</p>
+			</div>
 		</li>
+
 		<li>
-			<h3>Article Number Two</h3>
-			<p>This is a little blurb about the article to let you decide if you want to click</p>
+			<div class="article-number">
+				2
+			</div>
+
+			<div class="article-teaser">
+				<h3>Article Number Two</h3>
+
+				<p>This is a little blurb about the article to let you decide if you want to click</p>
+			</div>
 		</li>
+
 		<li>
-			<h3>Article Number Three</h3>
-			<p>This is a little blurb about the article to let you decide if you want to click</p>
+			<div class="article-number">
+				3
+			</div>
+
+			<div class="article-teaser">
+				<h3>Article Number Three</h3>
+
+				<p>This is a little blurb about the article to let you decide if you want to click</p>
+			</div>
 		</li>
+
 		<li>
-			<h3>Article Number Four</h3>
-			<p>This is a little blurb about the article to let you decide if you want to click</p>
+			<div class="article-number">
+				4
+			</div>
+
+			<div class="article-teaser">
+				<h3>Article Number Four</h3>
+
+				<p>This is a little blurb about the article to let you decide if you want to click</p>
+			</div>
 		</li>
+
 		<li>
-			<h3>Article Number Five</h3>
-			<p>This is a little blurb about the article to let you decide if you want to click</p>
+			<div class="article-number">
+				5
+			</div>
+
+			<div class="article-teaser">
+				<h3>Article Number Five</h3>
+
+				<p>This is a little blurb about the article to let you decide if you want to click</p>
+			</div>
 		</li>
 	</ol>
 </article-module>
+
+
--- projects/figma-wireframes/sections/modules/call-to-action.php
+++ projects/figma-wireframes/sections/modules/call-to-action.php
@@ -2,9 +2,9 @@
 	<div class="section-title">
 		<p>Rhoncus, accumsan dictum</p>
 
-		<h1>Etiam nulla lectus amet nunc 
-molestie at vulputate.</h1>
+		<h2>Etiam nulla lectus amet nunc 
+molestie at vulputate.</h2>
 	</div>
 
-	<a href="#" class="button">Get free quotes <?php include('sections/components/arrow-right.svg'); ?></a>
+	<a href="#" class="cta button">Get free quotes <?php include('sections/components/arrow-right.svg'); ?></a>
 </call-to-action>
