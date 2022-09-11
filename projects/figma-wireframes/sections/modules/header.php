fa45ecaed1860751760745768d9428196984c350
Tara <tara.haskamp@gmail.com>
2022-09-11 00:18:25 -0500

    Fix the header and footer section and column issue

--- projects/figma-wireframes/sections/modules/header.php
+++ projects/figma-wireframes/sections/modules/header.php
@@ -1,15 +1,17 @@
-<div class="inner-column">
-	<header class='page-header'>
-		<?php include('sections/components/logo.svg'); ?>
+<header>
+	<div class="inner-column">
+		<section class='page-header'>
+			<?php include('sections/components/logo.svg'); ?>
 
-		<nav class="header-nav">
-			<?php include('sections/components/nav.php');?>
-		</nav>
+			<nav class="header-nav">
+				<?php include('sections/components/nav.php');?>
+			</nav>
 
-		<nav class="account-nav">
-			<?php include('sections/components/account.php'); ?>
-		</nav>
-	</header>
-</div>
+			<nav class="account-nav">
+				<?php include('sections/components/account.php'); ?>
+			</nav>
+		</header>
+	</div>
+</header>
 
 <main>
