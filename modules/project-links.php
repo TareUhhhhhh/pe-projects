d17a75670fec651e31ac0c763201ab42440135bf
Tara <tara.haskamp@gmail.com>
2022-10-12 20:59:56 -0500

    Add button class to case study links

--- modules/project-links.php
+++ modules/project-links.php
@@ -1,4 +1,5 @@
 <section class='project-links'>
-	<a class='project-link' target='<?=$module['title']?>' href="<?=$module["siteLink"]?>">Live Site</a>
-	<a class='github-link' target="<?=$module['gitLink']?>" href="<?=$module['gitLink']?>">GitHub Link</a>
+	<a class='button project-link' target='<?=$module['title']?>' href="<?=$module["siteLink"]?>">Live Site</a>
+	
+	<a class='button github-link' target="<?=$module['gitLink']?>" href="<?=$module['gitLink']?>">GitHub Link</a>
 </section>
