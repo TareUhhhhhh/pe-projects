798cf1223b81bce234d935b707d8a8efa11627d2
Tara <tara.haskamp@gmail.com>
2022-10-14 20:20:07 -0500

    Update footer element structure

--- footer.php
+++ footer.php
@@ -5,20 +5,20 @@
 		<footer class="site-footer">
 			<div class="inner-column">
 
+				<site-footer>
+					<section class="contact"> 
+						<p>Feel free to <a href="mailto:tara@tareuhhhhhh.com" target="email">email me</a> to chat!</p>
 
-				<section class="contact"> 
-					<p class="email">Feel free to <a href="mailto:tara@tareuhhhhhh.com" target="email">email me</a> to chat!</p>
+						<p>You can also check out my writing that is posted to <a href="https://tarahaskamp.substack.com/" target="substack">Substack</a>.</p>
 
-					<p class="substack">You can also check out my writing that is posted to <a href="https://tarahaskamp.substack.com/" target="substack">Substack</a>.</p>
+						<p>I also keep my projects on <a href="https://github.com/TareUhhhhhh" target="github">Github</a> if you want to check them out!</p>	
 
-					<p class="github">I also keep my projects on <a href="https://github.com/TareUhhhhhh" target="github">Github</a> if you want to check them out!</p>	
-
-					<p>For my fellow nerds, you can also check out my <a href="?page=style-guide">style-guide</a></p>
-				</section>
+						<p>For my fellow nerds, you can also check out my <a href="?page=style-guide">style-guide</a></p>
+					</section>
+				</site-footer>
 
 				<?php // include("modules/theme-form.php");?>
-
-
+				
 			</div>
 		</footer>
 
