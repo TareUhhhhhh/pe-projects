b00dda9f32b248c1160737bf67481b19fe085248
Tara <tara.haskamp@gmail.com>
2022-10-14 19:59:07 -0500

    Fix previous change

--- modules/project-links.php
+++ modules/project-links.php
@@ -1,5 +1,5 @@
 <section class='project-links'>
-	<a class='button project-link' target='<?=$module['title']?>' href="../<?=$module["siteLink"]?>">Live Site</a>
+	<a class='button project-link' target='<?=$module['title']?>' href="<?=$module["siteLink"]?>">Live Site</a>
 	
 	<a class='button github-link' target="<?=$module['gitLink']?>" href="<?=$module['gitLink']?>">GitHub Link</a>
 </section>
