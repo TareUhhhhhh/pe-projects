2abff16790404891312c16586c22d8b405e90789
Tara <tara.haskamp@gmail.com>
2022-10-17 20:50:00 -0500

    Fix graphic diptych element structure

--- projects/tareuhhhhhh-gaming-v2/modules/graphic-diptych.php
+++ projects/tareuhhhhhh-gaming-v2/modules/graphic-diptych.php
@@ -3,9 +3,9 @@
 		<h1 class='intro-voice'><?=$pageContent["heading"]?></h1>
 
 		<p class='calm-voice'><?=$pageContent["intro"]?></p>
-
-		<picture>
-			<img src="<?=$pageContent['image']?>">
-		</picture>
 	</text-content>
+
+	<picture>
+		<img src="<?=$pageContent['image']?>">
+	</picture>
 </graphic-diptych>
