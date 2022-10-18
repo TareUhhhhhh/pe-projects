59cbbec81f91f8800dc9b788c79a37202308dbc6
Tara <tara.haskamp@gmail.com>
2022-10-17 22:41:44 -0500

    Update footer links

--- projects/tareuhhhhhh-gaming-v2/modules/site-map.php
+++ projects/tareuhhhhhh-gaming-v2/modules/site-map.php
@@ -6,20 +6,19 @@
 			<?php include("./icons/circle.php"); ?>
 		</a>
 		<a href='./'>Home</a>
-		<a href='404.php'>404 error</a>
 	</nav>
 
 	<nav class='user-menu'>
 		<h3>Account Menu</h3>
 
-		<a href='#'>Sign-in</a>
+		<a href='?page=account'>Sign-in</a>
 	</nav>
 
-	<nav class='legal-menu'>
-		<h3>Legal Menu</h3>
+	<nav class='game-menu'>
+		<h3>Games</h3>
 
-		<a href='#'>Legal thing a</a>
-		<a href='#'>Legal thing b</a>
+		<a href='?page=detail&game=after-dusk'>After Dusk</a>
+		<a href='?page=detail&game=scare-layers'>Scare Layers</a>
 	</nav>
 
 	<nav class='social-menu'>
