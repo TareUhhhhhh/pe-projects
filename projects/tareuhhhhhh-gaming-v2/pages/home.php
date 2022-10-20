002afd117f51481e056157866e8e247f2a848e87
Tara <tara.haskamp@gmail.com>
2022-10-20 00:31:43 -0500

    Fix spacing issues in code

--- projects/tareuhhhhhh-gaming-v2/pages/home.php
+++ projects/tareuhhhhhh-gaming-v2/pages/home.php
@@ -10,22 +10,31 @@
 <inner-column>
 
 	<?php 
-	$ctaHeading = "Our Newest Release";
-	$ctaText = "Action, adventure, drama, thrills, chills and scares! Not the latest Blumhouse film, but an all new game from TareUhhhhhh Gaming! It's called Spoops and Skills!";
-	$prompt = "Dare to play?";
-	include('modules/call-to-action.php'); ?>
+		$ctaHeading = "Our Newest Release";
+		$ctaText = "Action, adventure, drama, thrills, chills and scares! Not the latest Blumhouse film, but an all new game from TareUhhhhhh Gaming! It's called Spoops and Skills!";
+		$prompt = "Dare to play?";
+		include('modules/call-to-action.php'); 
+	?>
 
 </inner-column>
 </section>
 
 <section class="our-team">
-	<inner-column>
-		<?php 
+<inner-column>
+	<?php 
 		$pageContent["heading"] = "Join The Masters";
 		$pageContent["intro"] = "Our team of storytellers, environment creaters and character developers are the best of the best, in our own biased opinion. (That's why we hired them after all!) Join our growing company with ";
 		$pageContent['image'] = "images/gamer.jpg";
-		include("modules/graphic-diptych.php")?>
-	</inner-column>
+		include("modules/graphic-diptych.php")
+	?>
+</inner-column>
+</section>
+
+
+<section class="spotlight">
+<inner-column>
+	<?php include('modules/game-grid.php') ?>
+</inner-column>
 </section>
 
 
@@ -33,10 +42,11 @@
 <inner-column>
 
 	<?php 
-	$ctaHeading = "Not ready for the terror?";
-	$ctaText = "Our developers specialize in the creepy and crawly, but they know their way around the cutesy and adorable as well. Stay tuned for more from the land of Mary Mint and Joy.";
-	$prompt = "Care to play?";
-	include('modules/call-to-action.php'); ?>
+		$ctaHeading = "Not ready for the terror?";
+		$ctaText = "Our developers specialize in the creepy and crawly, but they know their way around the cutesy and adorable as well. Stay tuned for more from the land of Mary Mint and Joy.";
+		$prompt = "Care to play?";
+		include('modules/call-to-action.php'); 
+	?>
 
 </inner-column>
 </section>
