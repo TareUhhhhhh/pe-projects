8050fa8eb561e24b85c644d49b12afab96597375
Tara <tara.haskamp@gmail.com>
2022-10-21 00:07:59 -0500

    Fix spelling errors

--- projects/tareuhhhhhh-gaming-v2/pages/game-info.php
+++ projects/tareuhhhhhh-gaming-v2/pages/game-info.php
@@ -12,7 +12,7 @@ $games = [
 		],
 		"ageRating" => "M",
 		"description" => "Six friends have to live their boring, everyday lives until after dusk. Who will survive?",
-		"longDescription" => "In a remote work environment, people are strewn across the world and forced tostare at everyone's bored faces and occassionally a fluffy friend. Amid the awful greenscreen backgrounds of beaches and highrise apartments, one of their co-workers has a secret. Will you be able to find out in time?",
+		"longDescription" => "In a remote work environment, people are strewn across the world and forced to stare at everyone's bored faces and occassionally a fluffy friend. Amid the awful greenscreen backgrounds of beaches and highrise apartments, one of their co-workers has a secret. Will you be able to find out in time?",
 		"image" => "spooky.jpg",
 	],
 	[
@@ -26,7 +26,7 @@ $games = [
 		],
 		"ageRating" => "M",
 		"description" => "A dull artist can't paint a picture of his wife. Will you be able to prevent him from going crazy? (Doubt it)",
-		"longDescription" => "A boring artist wants to paint a picture of his wife. He has no talent and instead of admitting defeat he takes out his rage on everyone aound him. Can you sruvive his wrath?",
+		"longDescription" => "A boring artist wants to paint a picture of his wife. He has no talent and instead of admitting defeat he takes out his rage on everyone aound him. Can you survive his wrath?",
 		"image" => "creepy.jpg",
 	],
 ];
