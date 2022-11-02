f2393a4df83dc5f2dba12d2415ae90209315b0dd
Tara <tara.haskamp@gmail.com>
2022-11-01 22:56:05 -0500

    Add pseudocode and relocate todo app code

--- projects/todo-app/app.js
+++ projects/todo-app/app.js
@@ -1,73 +1,41 @@
+// FOOD APP - user specific food log for preferences
+
+//Each user will have their own **entries** recorded (local storage? How does this get specified per person and with all entries)
+
+//Each *entry* REQUIRES restaurant name, menu item and preference 
+// -- id auto added from fucntion
+//Optional - notes, price, 
+
+//Submit logs entry into local storage specific to user's list
 
 const foodApp = {
 
 	entries: [],
-	idNumber: 0,
 
-	print(note) {
+	print(note = "") {
 		console.log(`${note}`);
+		// console.log("Entry: ", this.entries);
 	},
 
 	add(restaurant, item, like) {
+		idNumber: 0;
 		const entry = {
 			id: this.idNumber++,
 			restaurant: restaurant,
 			item: item,
 		};
 
-		this.entries.push(entry); //What does this do?
-
-		this.print(`New entry -- `);
-		this.print(`id: ${this.id}`);
-		this.print(`Added Restaurant: "${restaurant}"`);
-		this.print(`Added Item: "${item}"`);
+		// this.entries.push(entry); //What does this do?
+		// this.print(`id: ${this.id}`);
+		this.print(`----New Entry----`);
+		this.print(`Restaurant: ${restaurant}`);
+		this.print(`Item: ${item}`);
 		this.print(`Would order again? ${like}`);
-		this.print(`----`);
-		localStorage.setItem("list", "TareUhhhhhh");
 	},
+
 }
 
 foodApp.add("Home", "Salmon", true);
 foodApp.add("Brewtop", "Chicken Tenders", false);
+foodApp.add("Hawaiian Bros", "Huli Huli Chicken", true);
 
-
-
-// const todoApp = {
-
-// 	tasks: [],
-// 	idNum: 0,
-
-// 	print(note = "") {
-// 		console.log(`--${note}`);
-// 		console.log("tasks: ", this.tasks);
-// 	},
-
-// 	add(task) {
-// 		const todo = {
-// 			id: this.idNum++,
-// 			task: task,
-// 		};
-// 		this.tasks.push(todo);
-// 		this.print(`Added "${task}"`);
-// 	},
-
-// 	remove(id) {
-// 		this.print(`Removed ${this.tasks[id].task}`);
-// 		this.tasks.splice(id, 1);
-// 		// delete tasks[id];
-// 	},
-
-// 	complete(id) {
-// 		this.tasks[id].complete = true;
-// 		this.print(`Completed ${this.tasks[id].task}`);
-// 	},
-// }
-
-// todoApp.add("Go to the store");
-// todoApp.add("Order birthday cake");
-// todoApp.add("Invite friends to party");
-
-// todoApp.remove(2);
-// todoApp.complete(1);
-
-// todoApp.add("Drink a glass of whiskey");
