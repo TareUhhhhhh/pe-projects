1d254cb979569ddbc2d105debf068aa169a2f52c
Tara <tara.haskamp@gmail.com>
2022-11-04 15:12:55 -0500

    Update Currently Looking module

--- pages/zen-responsive-garden.php
+++ pages/zen-responsive-garden.php
@@ -143,58 +143,61 @@
 
 	<currently-looking id="looking">
 		<div class="intro-content">
-			<h1 class="attention-voice">Stranger Ipsum Below</h1>
+			<h1 class="attention-voice">Currently Looking For...</h1>
 
-			<p class="calm-voice">Bitchin' Mouth-breather. Do you know anything about sensory deprivation tanks? Specifically how to build one? Nobody normal ever accomplished anything meaningful in this world. You’re pretty cute, you know that? Mistakes have been made.</p>
+			<p class="calm-voice">If you've made it this far, you must think I'm alright. This is a module that I lifted from a local site to replicate. They listed their job openings, so I will use it as an opportunity to list out what I'm looking for in a job! This is not a full list, nor is <em class="italic">everything</em> a requirement.</p>
 
-			<a href="#looking" class="button">Click Here</a>
+			<a href="?page=resume" class="button" target="resume">Resume Here</a>
 		</div>
 
 		<ul class="current-list calm-voice">
 			<li>
-				<h2 class="medium-voice">List Items 1</h2>
+				<h2 class="medium-voice">Job Titles</h2>
 
 				<ul>
-					<li>Item 1</li>
-					<li>Item 2</li>
-					<li>Item 3</li>
+					<li>Web Developer</li>
+					<li>Junior Web Developer</li>
+					<li>Front End Developer</li>
 				</ul>
 			</li>
 
 			<li>
-				<h2 class="medium-voice">List Items 2</h2>
+				<h2 class="medium-voice">Locations</h2>
 
 				<ul>
-					<li>Item 1</li>
+					<li>Kansas City Metro Area</li>
+					<li>Remote</li>
 				</ul>
 			</li>
 
 			<li>
-				<h2 class="medium-voice">List Items 3</h2>
+				<h2 class="medium-voice">Work Culture</h2>
 
 				<ul>
-					<li>Item 1</li>
-					<li>Item 2</li>
+					<li>Fun!</li>
+					<li>Helpful Team Environmet</li>
+					<li>Safe Place to Ask Questions</li>
+					<li>Growth-Minded</li>
 				</ul>
 			</li>
 
 			<li>
-				<h2 class="medium-voice">List Items 4</h1>
+				<h2 class="medium-voice">Job Responsibilities</h1>
 
 				<ul>
-					<li>Item 1</li>
-					<li>Item 2</li>
-					<li>Item 3</li>
+					<li>Building, Maintaining and Fixing websites using HTML5</li>
+					<li>Style websites with CSS</li>
+					<li>Utilize PHP and Javascript as needed for ease of use</li>
 				</ul>
 			</li>
 
-			<li>
+			<!-- <li>
 				<h2 class="medium-voice">List Items 5</h1>
 
 				<ul>
 					<li>Item 1</li>
 				</ul>
-			</li>
+			</li> -->
 		</ul>
 
 	</currently-looking>
