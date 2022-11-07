186ae40b9a13807a9e8c5d33edabbb7599ffcc99
Tara <tara.haskamp@gmail.com>
2022-11-06 18:11:54 -0600

    Add section video backgrounds

--- projects/tareuhhhhhh-gaming-v2/pages/account.php
+++ projects/tareuhhhhhh-gaming-v2/pages/account.php
@@ -3,5 +3,9 @@
 
 	<?php	include('modules/graphic-diptych.php'); ?>
 
+	<video autoplay muted loop class="myVideo">
+	  <source src="images/world.mp4" type="video/mp4">
+	</video>
+
 </inner-column>
 </section>
--- projects/tareuhhhhhh-gaming-v2/pages/news.php
+++ projects/tareuhhhhhh-gaming-v2/pages/news.php
@@ -3,6 +3,10 @@
 
 	<?php include('modules/graphic-diptych.php'); ?>
 
+	<video autoplay muted loop class="myVideo">
+	  <source src="images/news.mp4" type="video/mp4">
+	</video>
+
 </inner-column>
 </section>
 
