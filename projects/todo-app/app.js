486e371a47eeaa85df42e18fcce72d8097ea42f5
Tara <tara.haskamp@gmail.com>
2022-11-08 15:34:19 -0600

    Create new todo app for fresh practice

--- projects/todo-app/app.js
+++ projects/todo-app/app.js
@@ -1,41 +1,12 @@
-// FOOD APP - user specific food log for preferences
 
-//Each user will have their own **entries** recorded (local storage? How does this get specified per person and with all entries)
+const tasks = []; //Creates the array that gets stored in memory
 
-//Each *entry* REQUIRES restaurant name, menu item and preference 
-// -- id auto added from fucntion
-//Optional - notes, price, 
-
-//Submit logs entry into local storage specific to user's list
-
-const foodApp = {
-
-	entries: [],
-
-	print(note = "") {
-		console.log(`${note}`);
-		// console.log("Entry: ", this.entries);
-	},
-
-	add(restaurant, item, like) {
-		idNumber: 0;
-		const entry = {
-			id: this.idNumber++,
-			restaurant: restaurant,
-			item: item,
-		};
-
-		// this.entries.push(entry); //What does this do?
-		// this.print(`id: ${this.id}`);
-		this.print(`----New Entry----`);
-		this.print(`Restaurant: ${restaurant}`);
-		this.print(`Item: ${item}`);
-		this.print(`Would order again? ${like}`);
-	},
-
+function add(task) {
+	tasks.push(task); //This pushes the (task) into the tasks[] array
 }
 
-foodApp.add("Home", "Salmon", true);
-foodApp.add("Brewtop", "Chicken Tenders", false);
-foodApp.add("Hawaiian Bros", "Huli Huli Chicken", true);
+add("sleep");
+add("shower");
+add("eat");
 
+console.log("tasks: ", tasks); //This prints all of the items in the tasks[] array
---
+++ projects/todo-app/app.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+	<head></head>
+
+	<body>
+		<header></header>
+
+		<main>
+			<h1>To Dosies</h1>
+		</main>
+
+		<footer></footer>
+
+		<script src="app.js"></script>
+	</body>
+</html>
