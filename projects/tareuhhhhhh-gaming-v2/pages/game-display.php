7caf8b8fc551c8a4de3e9a828ebece491ca86b50
Tara <tara.haskamp@gmail.com>
2022-11-12 00:38:38 -0600

    Rename file

---
+++ projects/tareuhhhhhh-gaming-v2/pages/game-display.php
@@ -0,0 +1,25 @@
+<!-- parallax scrolling like on Bloober home page -->
+
+<?php 
+		$title = $game["title"];
+		$description = $game["description"];
+		$id = $game["id"];
+		$image = $game["image"];
+	?>
+
+<game-display style="background-image: url(images/<?=$image?>);">
+<inner-column>
+	<h1 class='attention-voice title'><?=$title?></h1>
+
+	<p class='calm-voice description'><?=$description?></p>
+
+	<a class="calm-voice" href="?page=detail&game=<?=$id?>">More Info</a>
+
+	<!-- <picture><img src="images/<?=$image?>"></picture> -->
+</inner-column>
+</game-display>
+
+
+
+
+
