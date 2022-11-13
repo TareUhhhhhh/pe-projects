87ce9a250db3dfc4de6192558846cbfbaaa18f3d
Tara <tara.haskamp@gmail.com>
2022-11-12 23:13:26 -0600

    Add flexbox to center section content

--- projects/tareuhhhhhh-gaming-v2/pages/game-display.php
+++ projects/tareuhhhhhh-gaming-v2/pages/game-display.php
@@ -9,11 +9,13 @@
 
 <game-display style="background-image: url(images/<?=$image?>);">
 <inner-column>
+	<text-content>
 		<h1 class='attention-voice title'><?=$title?></h1>
 
 		<p class='calm-voice description'><?=$description?></p>
 
 		<a class="calm-voice" href="?page=detail&game=<?=$id?>">More Info</a>
+	</text-content>
 </inner-column>
 </game-display>
 
