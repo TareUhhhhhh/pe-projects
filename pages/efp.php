266f6bc0125f184ad791724d108ffea2f5cde39b
Tara <tara.haskamp@gmail.com>
2022-12-01 15:13:05 -0600

    Add efp docs

--- pages/efp.php
+++ pages/efp.php
@@ -1,3 +1,3 @@
 <?php 
-	include('data/efpData.json');
+	// include('data/efpData.json');
 
---
+++ projects/efp/header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+
+<html lang='en'>
+	<head>
+		<meta charset='utf-8'>
+		<meta name='viewport' content='width=device-width, initial-scale=1'>
+		<title>Exercises for Programmers</title>
+		<meta name='description' content='Practice from the book by Brian Hogan'>
+		<meta property='og:image' content='images/default.png'>
+		<link rel='stylesheet' href='../../css/style.css'>
+	</head>
+
+	<body>
+
+		<header>
+			<div class="inner-column">
+				<nav>
+					<a href="?page=home">Home</a>
+				</nav>
+			</div>
+		</header>
+
+		<main>
