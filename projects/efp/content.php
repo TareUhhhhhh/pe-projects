8d77050f1ddebfe6a843b461a0cb32d5d5fabf13
Tara <tara.haskamp@gmail.com>
2022-12-01 22:03:42 -0600

    Add content page

--- projects/efp/content.php
+++ projects/efp/content.php
@@ -1,1 +1,9 @@
-content.php
+<section class="efp intro" id='intro'>
+<div class="inner-column">
+	<intro-content>
+		<h1 class='intro loud-voice'>Tara's Formulaic Stuff</h1>
+
+		<p class='intro normal-voice'>This is where I keep my collection of example forms. The information and inspiration for these forms comes from the book Exercises for Programmers by Brian Hogan.</p>
+	</intro-content>
+</div>
+</section>
