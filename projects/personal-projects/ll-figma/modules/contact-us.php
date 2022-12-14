ab41a70db9090401627e0ca81a30389ab57b184c
Tara <tara.haskamp@gmail.com>
2022-12-14 12:32:14 -0600

    Move contact us image

--- projects/personal-projects/ll-figma/modules/contact-us.php
+++ projects/personal-projects/ll-figma/modules/contact-us.php
@@ -1,4 +1,6 @@
 <contact-us>
+	<picture><img src="images/harry-swales.jpg" alt="Harry Swales with a microphone and red and white lights in the background"></picture>
+
 	<?php
 		$heading = "Get In Touch";
 		$paragraph = "New albums every single month, check out the newest & best from Snyder Recording artist, now available on Apple Music & Spotify.";
@@ -6,7 +8,5 @@
 		include('components/text-content.php');
 		include('components/contact-form.php');
 
-
 	?>
-	<picture><img src="images/harry-swales.jpg" alt="Harry Swales with a microphone and red and white lights behind him"></picture>
 </contact-us>
