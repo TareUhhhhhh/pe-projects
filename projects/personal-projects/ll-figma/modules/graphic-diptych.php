80d15be09c9b1c06ea884119a9674e94d3f7f8b8
Tara <tara.haskamp@gmail.com>
2022-12-14 21:56:32 -0600

    Simplify text content in graphic diptych

--- projects/personal-projects/ll-figma/modules/graphic-diptych.php
+++ projects/personal-projects/ll-figma/modules/graphic-diptych.php
@@ -1,13 +1,12 @@
 <graphic-diptych>
-	<picture><img src="images/red-hat.jpg" alt=""></picture>
+	<picture>
+		<img src="images/red-hat.jpg" alt="">
+	</picture>
 
 	<text-content>
-		<?php
-			$heading = 'Brent Faiyaz, So Far Gone';
-			$paragraph = 'Faiyaz began uploading his music onto SoundCloud in 2014 and moved from his hometown Columbia, Maryland to Charlotte, North Carolina before ultimately settling in Los Angeles, California to further his music career. On January 19, 2015, he released his debut single "Allure". On June 1, 2016, Faiyaz released "Invite Me", the lead single from his upcoming debut EP. On September 19, 2016, his 21st birthday, the EP entitled A.M. Paradox was released and received with positive reviews from music critics. On January 26, 2017, Sonder released their debut EP Into.';
+		<h2 class="attention-voice">Brent Faiyaz, So Far Gone</h2>
 
-			include ('components/text-content.php');
-		?>
+		<p>Faiyaz began uploading his music onto SoundCloud in 2014 and moved from his hometown Columbia, Maryland to Charlotte, North Carolina before ultimately settling in Los Angeles, California to further his music career. On January 19, 2015, he released his debut single "Allure". On June 1, 2016, Faiyaz released "Invite Me", the lead single from his upcoming debut EP. On September 19, 2016, his 21st birthday, the EP entitled A.M. Paradox was released and received with positive reviews from music critics. On January 26, 2017, Sonder released their debut EP Into.</p>
 
 		<a href="#" class="button">Learn More</a>
 	</text-content>
