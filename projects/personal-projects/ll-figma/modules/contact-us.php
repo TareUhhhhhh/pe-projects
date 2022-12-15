7307b73437e152a8da8530e4424bf6b99d5378a3
Tara <tara.haskamp@gmail.com>
2022-12-14 22:32:45 -0600

    Simplify text content in contact us section

--- projects/personal-projects/ll-figma/modules/contact-us.php
+++ projects/personal-projects/ll-figma/modules/contact-us.php
@@ -1,12 +1,9 @@
 <contact-us>
 	<picture><img src="images/harry-swales.jpg" alt="Harry Swales with a microphone and red and white lights in the background"></picture>
 
-	<?php
-		$heading = "Get In Touch";
-		$paragraph = "New albums every single month, check out the newest & best from Snyder Recording artist, now available on Apple Music & Spotify.";
-
-		include('components/text-content.php');
-		include('components/contact-form.php');
-
-	?>
+	<h2 class="attention-voice">Get In Touch</h2>
+	
+	<p>New albums every single month, check out the newest & best from Snyder Recording artist, now available on Apple Music & Spotify.</p>
+	
+	<?php include('components/contact-form.php');?>
 </contact-us>
