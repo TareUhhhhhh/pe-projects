87311a7ee5691fe74ab58867c52b379ca8d5fe56
Tara <tara.haskamp@gmail.com>
2022-12-15 21:00:42 -0600

    Update contact section layout

--- projects/personal-projects/ll-figma/modules/components/contact-form.php
+++ projects/personal-projects/ll-figma/modules/components/contact-form.php
@@ -1,63 +1,62 @@
-<contact-form>
-	<?php
-		if (isset($_POST['first'])) {
-			$first = $_POST['first'];
-		}
-		else {
-			$first = "First Name";
-		}
 
-		if (isset($_POST['last'])) {
-			$last = $_POST['last'];
-		}
-		else {
-			$last = "Last Name";
-		}
+<?php
+	if (isset($_POST['first'])) {
+		$first = $_POST['first'];
+	}
+	else {
+		$first = "First Name";
+	}
 
-		if (isset($_POST['email'])) {
-			$email = $_POST['email'];
-		}
-		else {
-			$email = "Email Address";
-		}
+	if (isset($_POST['last'])) {
+		$last = $_POST['last'];
+	}
+	else {
+		$last = "Last Name";
+	}
 
-
-	?>
+	if (isset($_POST['email'])) {
+		$email = $_POST['email'];
+	}
+	else {
+		$email = "Email Address";
+	}
 
 
+?>
 
-	<form action="POST">
-		<div class="form-field">
-			<label for="first-name" class="quiet-voice">First Name</label>
-			<input type="text" id="first-name" placeholder="<?=$first?>" required>
-		</div>
 
-		<div class="form-field">
-			<label for="last-name" class="quiet-voice">Last Name</label>
-			<input type="text" id="last-name" placeholder="<?=$last?>" required>
-		</div>
 
-		<div class="form-field">
-			<label for="email" class="quiet-voice">Email Address</label>
-			<input type="text" id="email" placeholder="<?=$email?>" required>
-		</div>
+<form class="contact-form" action="POST">
+	<div class="form-field">
+		<label for="first-name" class="quiet-voice">First Name</label>
+		<input type="text" id="first-name" placeholder="<?=$first?>" required>
+	</div>
 
-		<div class="form-field reason">
-			<label for="reason" class="quiet-voice">Reason For Contacting</label>
+	<div class="form-field">
+		<label for="last-name" class="quiet-voice">Last Name</label>
+		<input type="text" id="last-name" placeholder="<?=$last?>" required>
+	</div>
 
-			<select name="reason" id="reason-select" required>
-				<option value="">Select One</option>
-				<option value="one">Reason Number 1</option>
-				<option value="two">Reason Number 2</option>
-			</select>
-		</div>
+	<div class="form-field">
+		<label for="email" class="quiet-voice">Email Address</label>
+		<input type="text" id="email" placeholder="<?=$email?>" required>
+	</div>
 
-		<div class="form-field message">
-			<label for="message" class="quiet-voice">Message</label>
+	<div class="form-field reason">
+		<label for="reason" class="quiet-voice">Reason For Contacting</label>
 
-			<textarea type="textarea" name="message" id="message" rows="4" columns="50"></textarea>
-		</div>
+		<select name="reason" id="reason-select" required>
+			<option value="">Select One</option>
+			<option value="one">Reason Number 1</option>
+			<option value="two">Reason Number 2</option>
+		</select>
+	</div>
 
-		<button input='submit' name='submitted' class="button">Learn More</button>
-	</form>
-</contact-form>
+	<div class="form-field message">
+		<label for="message" class="quiet-voice">Message</label>
+
+		<textarea type="textarea" name="message" id="message" rows="4" columns="50"></textarea>
+	</div>
+
+	<button input='submit' name='submitted' class="button">Learn More</button>
+</form>
--- projects/personal-projects/ll-figma/modules/contact-us.php
+++ projects/personal-projects/ll-figma/modules/contact-us.php
@@ -1,9 +1,11 @@
 <contact-us>
 	<picture><img src="images/harry-swales.jpg" alt="Harry Swales with a microphone and red and white lights in the background"></picture>
 
-	<h2 class="attention-voice">Get In Touch</h2>
-	
-	<p>New albums every single month, check out the newest & best from Snyder Recording artist, now available on Apple Music & Spotify.</p>
-	
-	<?php include('components/contact-form.php');?>
+	<contact-form>
+		<h2 class="attention-voice">Get In Touch</h2>
+		
+		<p>New albums every single month, check out the newest & best from Snyder Recording artist, now available on Apple Music & Spotify.</p>
+		
+		<?php include('components/contact-form.php');?>
+	</contact-form>
 </contact-us>
