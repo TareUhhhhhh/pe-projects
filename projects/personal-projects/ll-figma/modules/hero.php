32710257e776c9c9113268cdc25038ab1378262d
Tara <tara.haskamp@gmail.com>
2022-12-15 22:34:07 -0600

    Add keyframes for hero banner

--- projects/personal-projects/ll-figma/modules/hero.php
+++ projects/personal-projects/ll-figma/modules/hero.php
@@ -1,4 +1,5 @@
 <hero-banner>
+
 	<h1 class="loud-voice">A.M. Paradox</h1>
 
 	<p class="calm-voice">Christopher Brent Wood, better known by his stage name Brent Faiyaz, is an American singer and record producer. He released his debut project, an extended play entitled A.M. Paradox, in 2016.</p>
