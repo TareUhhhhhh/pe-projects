ea3f7792799e4465e6cf8101598e01315a4ce174
Tara <tara.haskamp@gmail.com>
2022-12-16 22:55:38 -0600

    Rename hero banner file

---
+++ projects/personal-projects/ll-figma/modules/hero-banner.php
@@ -0,0 +1,13 @@
+<hero-banner>
+
+	<h1 class="loud-voice">A.M. Paradox</h1>
+
+	<p>Christopher Brent Wood, better known by his stage name Brent Faiyaz, is an American singer and record producer. He released his debut project, an extended play entitled A.M. Paradox, in 2016.</p>
+
+	<a href="#" class="video-link">
+		<div class="play-button">
+			<?php include('images/icons/basic/play-button-triangle.svg'); ?>
+			<p>Watch Video</p>
+		</div>
+	</a>
+</hero-banner>
