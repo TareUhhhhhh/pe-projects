bc47e75bf6426e919016bb1ad80274e4332c994c
Tara <tara.haskamp@gmail.com>
2022-12-17 21:04:45 -0600

    Set center card as default

--- projects/personal-projects/ll-figma/modules/slider.php
+++ projects/personal-projects/ll-figma/modules/slider.php
@@ -1,4 +1,4 @@
-<release-slider data-flickity='{ "pageDots": false }'>
+<release-slider data-flickity='{ "pageDots": false, "initialIndex": 1 }'>
 	<?php 
 		foreach([1, 2, 3] as $card) {
 			include('components/slider-card.php');
