1d446055758ca7123f4c91e8b50c30c373b9b52e
Tara <tara.haskamp@gmail.com>
2022-12-19 01:26:21 -0600

    Add non breaking space to spotlight heading

--- projects/personal-projects/ll-figma/modules/graphic-diptych.php
+++ projects/personal-projects/ll-figma/modules/graphic-diptych.php
@@ -3,7 +3,7 @@
 		<img src="images/red-hat.jpg" alt="">
 	</picture>
 
-	<h2 class="attention-voice">Brent Faiyaz, So Far Gone</h2>
+	<h2 class="attention-voice">Brent Faiyaz, So Far&nbsp;Gone</h2>
 
 	<p>Faiyaz began uploading his music onto SoundCloud in 2014 and moved from his hometown Columbia, Maryland to Charlotte, North Carolina before ultimately settling in Los Angeles, California to further his music career. On January 19, 2015, he released his debut single "Allure". On June 1, 2016, Faiyaz released "Invite Me", the lead single from his upcoming debut EP. On September 19, 2016, his 21st birthday, the EP entitled A.M. Paradox was released and received with positive reviews from music critics. On January 26, 2017, Sonder released their debut EP Into.</p>
 
