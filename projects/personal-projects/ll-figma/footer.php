77917c411bd4c2f9c1b01c69c454cc7579ca69e7
Tara <tara.haskamp@gmail.com>
2022-12-19 22:25:50 -0600

    Add footer section

--- projects/personal-projects/ll-figma/footer.php
+++ projects/personal-projects/ll-figma/footer.php
@@ -2,7 +2,7 @@
 
 		<footer>
 		<div class="inner-column">
-			<nav>
+			<footer-content>
 				<location-info>
 					<?php  include('images/logos/logo-white.svg');?>
 					<p class="calm-voice">8521 W Fairfax Rd</p>
@@ -10,51 +10,52 @@
 					<p class="calm-voice">(909) 505-4302</p>
 				</location-info>
 
-				<explore-nav>
-					<p class="calm-voice upper">Explore</p>
-					<ul>
-						<li><a href="#">About Us</a></li>
-						<li><a href="#">Our Story</a></li>
-						<li><a href="#">Team</a></li>
-						<li><a href="#">Careers</a></li>
-						<li><a href="#">FAQ</a></li>
-					</ul>
-				</explore-nav>
+				<nav>
+					<explore-nav>
+						<p class="calm-voice upper">Explore</p>
+						<ul>
+							<li><a href="#">About Us</a></li>
+							<li><a href="#">Our Story</a></li>
+							<li><a href="#">Team</a></li>
+							<li><a href="#">Careers</a></li>
+							<li><a href="#">FAQ</a></li>
+						</ul>
+					</explore-nav>
 
-				<music-nav>
-					<p class="calm-voice upper">Music</p>
-					<ul>
-						<li><a href="#">Missing Out</a></li>
-						<li><a href="#">Trapsoul</a></li>
-						<li><a href="#">Sonder Sons</a></li>
-						<li><a href="#">Paradise Today</a></li>
-						<li><a href="#">Needed</a></li>
-					</ul>
-				</music-nav>
+					<music-nav>
+						<p class="calm-voice upper">Music</p>
+						<ul>
+							<li><a href="#">Missing Out</a></li>
+							<li><a href="#">Trapsoul</a></li>
+							<li><a href="#">Sonder Sons</a></li>
+							<li><a href="#">Paradise Today</a></li>
+							<li><a href="#">Needed</a></li>
+						</ul>
+					</music-nav>
 
-				<artists-nav>
-					<p class="calm-voice upper">Artists</p>
-					<ul>
-						<li><a href="#">Brent Faiyaz</a></li>
-						<li><a href="#">Drake</a></li>
-						<li><a href="#">Bryson Ford</a></li>
-						<li><a href="#">Young Ford</a></li>
-						<li><a href="#">Roy Fresco</a></li>
-					</ul>
-				</artists-nav>
+					<artists-nav>
+						<p class="calm-voice upper">Artists</p>
+						<ul>
+							<li><a href="#">Brent Faiyaz</a></li>
+							<li><a href="#">Drake</a></li>
+							<li><a href="#">Bryson Ford</a></li>
+							<li><a href="#">Young Ford</a></li>
+							<li><a href="#">Roy Fresco</a></li>
+						</ul>
+					</artists-nav>
 
-				<records-nav>
-					<p class="calm-voice upper">Records</p>
-					<ul>
-						<li><a href="#">What a Time</a></li>
-						<li><a href="#">Coming Home</a></li>
-						<li><a href="#">Always First</a></li>
-						<li><a href="#">So Far Gone</a></li>
-						<li><a href="#">Big Bluez</a></li>
-					</ul>
-				</records-nav>
-
-			</nav>
+					<records-nav>
+						<p class="calm-voice upper">Records</p>
+						<ul>
+							<li><a href="#">What a Time</a></li>
+							<li><a href="#">Coming Home</a></li>
+							<li><a href="#">Always First</a></li>
+							<li><a href="#">So Far Gone</a></li>
+							<li><a href="#">Big Bluez</a></li>
+						</ul>
+					</records-nav>
+				</nav>
+			</footer-content>
 
 			<end-note>
 				<legal-nav>
