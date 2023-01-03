f92bb5013382d43092df17995a044342985aa6b5
Tara <tara.haskamp@gmail.com>
2023-01-03 17:58:30 -0600

    Move Javascript practice files to pe projects

---
+++ projects/javascript-practice/more-scripts.js
@@ -0,0 +1,102 @@
+// // Create Variables
+// var list = "Cookies";
+// var items = ["Sugar", "Chocolate Chip"];
+// var type = "Bars";
+
+// // Log a message to the console with the variable 
+// console.log("The " + list + " we will be bringing are " + items + " " + type);
+
+// // Create variables again
+// var person = "Tara";
+// var drink = "water";
+// var food = "cookies";
+// var activity = "school work";
+
+// // Create the message AS a variable
+// var message = "Currently, " + person + " is drinking " + drink + ", eating " + food + " and doing " + activity + ".";
+
+// // Log the message variable to the console
+// console.log(message);
+
+// // Add the message to the page html
+// var p = document.createElement("p");
+// p.textContent = message;
+
+// document.body.appendChild(p)
+
+// Function to create paragraphs
+// function makeElement(element, stuff) {
+// 	var element = document.createElement(element);
+// 	element.textContent = stuff;
+// 	document.body.appendChild(element);
+// }
+
+// makeElement("h3", "Adding an H3!");
+
+
+// ^^^ FIRST PRACTICE GO ROUND ^^^
+
+
+// -----------------------
+// // Create Variables
+var show = "Big Bang Theory";
+var characters = {
+	maleCharacters: [
+		"Sheldon", 
+		"Leonard", 
+		"Raj", 
+		"Howard"
+	],
+	femaleCharacters: [
+		"Penny",
+		"Bernadette",
+		"Amy"
+	]
+}
+
+var message = show + " is a show with many characters. " + characters.maleCharacters + " are the male characters. " + characters.femaleCharacters + " are the female characters.";
+
+
+
+
+// // Log a message to the console with the variable 
+console.log(show + " is a show with many characters. " + characters.maleCharacters + " are the male characters. " + characters.femaleCharacters + " are the female characters.");
+
+// // Create variables again
+
+
+// // Create the message AS a variable
+
+
+// // Log the message variable to the console
+
+
+
+// // Add the message to the page html
+
+
+// // Function to create paragraphs
+
+
+// -------------------
+// // Create Variables
+
+
+
+// // Log a message to the console with the variable 
+
+
+// // Create variables again
+
+
+// // Create the message AS a variable
+
+
+// // Log the message variable to the console
+
+
+
+// // Add the message to the page html
+
+
+// // Function to create paragraphs
---
+++ projects/javascript-practice/scripts.js
@@ -0,0 +1,131 @@
+//console.log
+var name = "TareUhhhhhh";
+var age = 33;
+var mood = "Versatile";
+console.log("Name: ", name, "Age: ", age, "Mood: ", mood);
+
+//function
+function shoutName() {
+	console.log("My name is " + name + "!");
+}
+
+shoutName("Tara");
+
+//nested function
+function petGreeting() {
+	var petName = prompt("What is your pets name?");
+
+	function animal() {
+		var petType = prompt("What kind of animal is " + petName + "?");
+
+		if (petType) {
+			alert("Tell your " + petType + " " + petName + " that I said hello!");
+		}
+
+		else {
+			alert("You don't know what kind of animal " + petName + " is??");
+			animal();
+		}
+	}
+
+	if (petName) {
+			animal();
+		}
+
+	else {
+		alert("I think you forgot something.");
+		petGreeting();
+	}
+}
+
+// petGreeting();
+
+//array
+var thisArray = ["apetite", "bug smash", "caterpillar", "dingus"];
+
+// for ( var each = 0; each < thisArray.length; each++) {
+// 	console.log("!!", thisArray[each]);
+// }
+
+function coolThings(array, funktion) {
+	for ( var i = 0; i < array.length; i++) {
+		funktion(array[i]);
+	}
+}
+
+coolThings( thisArray, shoutName);
+
+//foreach
+var myFaves = [
+	"skeleton", 
+	"ghost", 
+	"bat", 
+	"fox", 
+	"skull", 
+	"bones",
+	"cat"
+]
+
+var myFavorites = [
+	{
+		name: "skeleton",
+		type: "prop",
+	}, 
+	{
+		name: "ghost",
+		type: "ethereal",
+	},
+	{
+		name: "bat",
+		type: "animal",
+	},
+	{
+		name: "fox",
+		type: "animal",
+	},
+	{
+		name: "skull",
+		type: "prop",
+	},
+	{
+		name: "bones",
+		type: "prop",
+	},
+	{
+		name: "cat",
+		type: "pet",
+	},
+]
+
+function loggs(item, index) {
+	var index = index + 1;
+	console.log(index, item);
+}
+
+// myFaves.forEach( loggs );
+
+myFavorites.forEach( function(item) {
+	if (item.type != "prop") {
+		console.log(item.name);
+	}
+});
+
+var animals = myFavorites.filter( function(item) {
+	if (item.type == "animal");
+} );
+
+console.log(animals);
+
+function buildList(item, heading) {
+	var h1 = document.createElement(h1);
+	h1.textContent = "heading";
+	document.body.apendChild(h1);
+	var ul = document.createElement(ul);
+
+	item.forEach( function(item) {
+		var li = document.createElement(li);
+		
+	})
+	
+
+}
