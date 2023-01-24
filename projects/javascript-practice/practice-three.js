afb0671eea03378766993d26952adb2ab4eb7ebb
Tara <tara.haskamp@gmail.com>
2023-01-23 21:56:43 -0600

    Add new javascript practice files

---
+++ projects/javascript-practice/practice-three.js
@@ -0,0 +1,15 @@
+let firstNumber = 5;
+let secondNumber = 9;
+
+function compare(first, second) {
+	if (first > second) {
+		let highest = first;
+		console.log(highest);
+	}
+	else {
+		let highest = second;
+		console.log(highest);
+	}
+}
+
+compare(firstNumber, secondNumber);
