4025333c4a6b596aecf12e2cd31ba10750b95e33
Tara <tara.haskamp@gmail.com>
2023-01-25 14:21:15 -0600

    Clean up code spacing

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php
-	// global functions
 	include("functions.php");
-
-	// the "database"
 	include('page-data.php');
-
-
 	include('header.php');
-	
+
 	renderPage($activePage);
-	
+
 	include('footer.php');
