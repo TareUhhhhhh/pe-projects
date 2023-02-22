5205d07914a51519bd963fbe69dd890528680b37
Tara <tara.haskamp@gmail.com>
2023-02-21 20:45:51 -0600

    Add Distractify copy files

---
+++ projects/distractify/components/footer.php
@@ -0,0 +1,7 @@
+	</main>
+
+	<footer class="site-footer">
+	<div class="inner-column">
+		<p class="mono-voice">Copyright and Legal Info stuff</p>
+	</div>
+	</footer>
---
+++ projects/distractify/components/header.php
@@ -0,0 +1,15 @@
+
+<header>
+	<div class="inner-column">
+		<nav class="site-header-nav">
+			<a href="#">Home</a>
+			<a href="#">Second Link</a>
+		</nav>
+
+		<nav class="account-header-nav">
+			<a href="#">Account Info</a>
+		</nav>
+	</div>
+</header>
+
+<main>
