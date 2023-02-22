370644814587d17cf4f896152b5c5ed4f90506f5
Tara <tara.haskamp@gmail.com>
2023-02-21 20:46:08 -0600

    Create Once Upon a Chef copy files

---
+++ projects/once-upon-a-chef/components/content.php
@@ -0,0 +1,51 @@
+<section class="site-intro" id="site-intro">
+<div class="inner-column">
+	<h1 class="page-title">This is a "Skeleton Template"</h1>
+
+	<p>This is how a site will look with some text on it.</p>
+</div>
+</section>
+
+<section class="card-gallery">
+<div class="inner-column">
+	<gallery-intro>
+		<h2>This is how a section would look with some link cards</h2>
+	</gallery-intro>
+
+	<card-gallery>
+		<link-card>
+			<h3>Card Title #1</h3>
+
+			<p>A short description of the point of this card/link.</p>
+
+			<a href="#">Click Here</a>
+		</link-card>
+
+		<link-card>
+			<h3>Longer Card Title Number Two</h3>
+
+			<p>A short description of the point of this card/link.</p>
+
+			<a href="#">Click Here</a>
+		</link-card>
+
+		<link-card>
+			<h3>Another Card Title</h3>
+
+			<p>A short description of the point of this card/link.</p>
+
+			<a href="#">Click Here</a>
+		</link-card>
+	</card-gallery>
+</div>
+</section>
+
+<section class="call-to-action">
+<div class="inner-column">
+	<h2>Call To Action Heading</h2>
+
+	<p>A teaser paragraph about the Call To Action</p>
+
+	<a href="#">Click Here to Learn More</a>
+</div>
+</section>
