986c355f57b4d094a3c9624caed98683bf65ce2c
Tara <tara.haskamp@gmail.com>
2023-02-24 19:35:49 -0600

    Add header nav links

--- projects/site-dupes/distractify/components/header.php
+++ projects/site-dupes/distractify/components/header.php
@@ -2,12 +2,22 @@
 <header>
 	<div class="inner-column">
 		<nav class="site-header-nav">
-			<a href="#">Home</a>
-			<a href="#">Second Link</a>
+			<a href="#">Entertainment</a>
+			<a href="#">Television</a>
+			<a href="#">Trending</a>
 		</nav>
 
 		<nav class="account-header-nav">
-			<a href="#">Account Info</a>
+			<a href="#">Influencers</a>
+			<a href="#">Gaming</a>
+			<a href="#">Exclusives</a>
+		</nav>
+
+		<nav class="account-header-socials">
+			<a href="#">Facebook</a>
+			<a href="#">Instagram</a>
+			<a href="#">Twitter</a>
+			<a href="#">Email</a>
 		</nav>
 	</div>
 </header>
