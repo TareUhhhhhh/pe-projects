dd5825b47b5b56e1066d6e82b8737c0d11a72cc5
Tara <tara.haskamp@gmail.com>
2023-02-24 19:29:06 -0600

    Update meta data info

--- projects/site-dupes/distractify/components/head.php
+++ projects/site-dupes/distractify/components/head.php
@@ -4,8 +4,8 @@
 	<head>
 		<meta charset='utf-8'>
 		<meta name='viewport' content='width=device-width, initial-scale=1'>
-		<title>Skeleplate</title>
-		<meta name='description' content='[[insert description]]'>
+		<title>Distractify Copy</title>
+		<meta name='description' content='I made a visual copy of a Distractify article page.'>
 		<meta property='og:image' content='[[insert image]]'>
 		<link rel='stylesheet' href='css/style.css'>
 	</head>
