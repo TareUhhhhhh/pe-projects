443d729e73b5c6f7484a201f9357e87d42db56da
Tara <tara.haskamp@gmail.com>
2023-05-17 16:56:14 -0500

    Add section heading to text content module

--- modules/text-content.php
+++ modules/text-content.php
@@ -5,6 +5,10 @@
 		<h3 class="section-title attention-voice"><?=$module['title']?></h3>
 	<?php } ?>
 
+	<?php if(isset($module['section-heading'])) { ?>
+		<h4 class="section-title medium-voice"><?=$module['section-heading']?></h4>
+	<?php } ?>
+
 	<?php
 		foreach ($module['paragraph'] as $paragraph) { 
 			echo "<p>$paragraph</p>";
